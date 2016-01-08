c58169f2e5d12cb90c7a3a81d8f7463e02d82894
tobiasweinzierl <tobias.weinzierl@durham.ac.uk>
2016-01-08 11:08:15 +0000

    Ready to write an application-specific makefile

--- Code/Toolkit/src/eu/exahype/SetupBuildEnvironment.java
+++ Code/Toolkit/src/eu/exahype/SetupBuildEnvironment.java
@@ -18,7 +18,20 @@ public class SetupBuildEnvironment extends DepthFirstAdapter {
       java.io.File logFile = new java.io.File(_directoryAndPathChecker.outputDirectory.getAbsolutePath() + "/Makefile");
 
       java.io.BufferedWriter writer = new java.io.BufferedWriter(new java.io.FileWriter(logFile));
-      writer.write("Hello world!");
+      writer.write("PEANO_PATH=" + _directoryAndPathChecker.peanoPath.getAbsolutePath() + "\n");
+      writer.write("TARCH_PATH=" + _directoryAndPathChecker.tarchPath.getAbsolutePath() + "\n");
+      writer.write("MULTISCALELINKEDCELL_PATH=" + _directoryAndPathChecker.multiscalelinkedcellPath.getAbsolutePath() + "\n");
+      writer.write("EXAHYPE_PATH=" + _directoryAndPathChecker.exahypePath.getAbsolutePath() + "\n");
+      writer.write("PROJECT_PATH=" + _directoryAndPathChecker.outputDirectory.getAbsolutePath() );
+      writer.write("\n\n\n\n");
+      writer.write("-include " + _directoryAndPathChecker.exahypePath.getAbsolutePath() + "/Makefile\n");
+      writer.write("\n\n\n\n");
+      writer.write("all: \n");
+      writer.write("\t@echo " + node.getName() + "\n");
+      writer.write("\t@echo =================\n");
+      writer.write("\t@echo An ExaHyPE solver\n");
+
+      System.out.print ("store pathes in makefile ... ok" );      
       
       writer.close();
 	} 
