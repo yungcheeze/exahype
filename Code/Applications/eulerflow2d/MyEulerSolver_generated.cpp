d0c259c69d7f0cf2e459bafea053822daaa16daf
tobiasweinzierl <tobias.weinzierl@durham.ac.uk>
2016-02-03 17:17:22 +0000

    Merged them (I hope).

---
+++ Code/Applications/eulerflow2d/MyEulerSolver_generated.cpp
@@ -0,0 +1,53 @@
+// ==============================================
+// Please do not change the implementations below
+// =============================---==============
+#include "MyEulerSolver.h"
+#include "kernels/aderdg/generic/Kernels.h"
+
+
+
+#include "kernels/aderdg/generic/Kernels.h"
+
+
+void Euler2d::MyEulerSolver::spaceTimePredictor( double* lQi, double* lFi, double* lQhi, double* lFhi, double* lQhbnd, double* lFhbnd, const double* const luh, const tarch::la::Vector<DIMENSIONS,double>& dx, const double dt ) {
+   kernels::aderdg::generic::spaceTimePredictor<flux>( lQi, lFi, lQhi, lFhi, lQhbnd, lFhbnd, luh, dx, dt, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
+void Euler2d::MyEulerSolver::solutionUpdate(double* luh, const double* const lduh, const tarch::la::Vector<DIMENSIONS,double>& dx, const double dt) {
+   kernels::aderdg::generic::solutionUpdate( luh, lduh,dx, dt, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
+void Euler2d::MyEulerSolver::volumeIntegral(double* lduh, const double* const lFhi, const tarch::la::Vector<DIMENSIONS,double>& dx) {
+   kernels::aderdg::generic::volumeIntegral( lduh, lFhi,dx, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
+void Euler2d::MyEulerSolver::surfaceIntegral(double* lduh, const double* const lFhbnd, const tarch::la::Vector<DIMENSIONS,double>& dx) {
+   kernels::aderdg::generic::surfaceIntegral( lduh, lFhbnd, dx, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
+void Euler2d::MyEulerSolver::riemannSolver(double* FL, double* FR, const double* const QL, const double* const QR, const double dt, const int normalNonZeroIndex) {
+   kernels::aderdg::generic::riemannSolver<eigenvalues>( FL, FR, QL, QR, dt, normalNonZeroIndex, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
+double Euler2d::MyEulerSolver::stableTimeStepSize(const double* const luh, const tarch::la::Vector<DIMENSIONS,double>& dx) {
+   return kernels::aderdg::generic::stableTimeStepSize<eigenvalues>( luh, dx, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
+void Euler2d::MyEulerSolver::initialCondition(double* luh, const tarch::la::Vector<DIMENSIONS,double>& center, const tarch::la::Vector<DIMENSIONS,double>& dx) {
+   kernels::aderdg::generic::initialCondition<initialValues>( luh, center, dx, getNumberOfVariables(), getNodesPerCoordinateAxis() );
+}
+
+
+
