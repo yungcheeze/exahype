bc9e502670fff6d6127ce64e80aa493f687ad50e
di25cox <dominic.e.charrier@durham.ac.uk>
2016-02-16 12:56:21 +0000

    Added assertions.

--- Code/ExaHyPE/exahype/mappings/NewTimeStep.cpp
+++ Code/ExaHyPE/exahype/mappings/NewTimeStep.cpp
@@ -368,17 +368,28 @@ void exahype::mappings::NewTimeStep::enterCell(
       p->setCorrectorTimeStepSize(solve->getCorrectorTimeStepSize());
       p->setPredictorTimeStamp   (solve->getPredictorTimeStamp   ());
       p->setPredictorTimeStepSize(solve->getPredictorTimeStepSize());
+
+      assertionNumericalEquals1(p->getCorrectorTimeStamp()   ,solve->getCorrectorTimeStamp(),   1e-12); // todo precision
+      assertionNumericalEquals1(p->getCorrectorTimeStepSize(),solve->getCorrectorTimeStepSize(),1e-12);
+      assertionNumericalEquals1(p->getPredictorTimeStamp()   ,solve->getPredictorTimeStamp(),   1e-12);
+      assertionNumericalEquals1(p->getPredictorTimeStepSize(),solve->getPredictorTimeStepSize(),1e-12);
     }
     if (!solve->isCorrectorTimeLagging()) {
       p->setCorrectorTimeStamp   (p->getPredictorTimeStamp   ());
       p->setCorrectorTimeStepSize(p->getPredictorTimeStepSize());
     }
 
-    // todo remove
-    assertion(solve->getTimeStepping()==exahype::solvers::Solve::GLOBAL);
-//    assertion(solve->isCorrectorTimeLagging()==true);
-//    assertion(p->getCorrectorTimeStamp()   ==solve->getCorrectorTimeStamp());
-//    assertion(p->getCorrectorTimeStepSize()==solve->getCorrectorTimeStepSize());
+#if defined(Debug) || defined(Asserts)
+    if (solve->getTimeStepping()==exahype::solvers::Solve::GLOBAL && !solve->isCorrectorTimeLagging()) {
+      // Note that the solve time stamps and time step sizes are not modified if corrector time lagging
+      // is deactivated. Thus, solve->getPredictor... and solve->getCorrector... are not the same in general
+      // for any value of solve->isCorrectorTimeLagging().
+      assertionNumericalEquals1(p->getPredictorTimeStamp()   ,solve->getPredictorTimeStamp(),   1e-12); // todo precision
+      assertionNumericalEquals1(p->getPredictorTimeStepSize(),solve->getPredictorTimeStepSize(),1e-12);
+      assertionNumericalEquals1(p->getCorrectorTimeStamp()   ,solve->getPredictorTimeStamp(),   1e-12);
+      assertionNumericalEquals1(p->getCorrectorTimeStepSize(),solve->getPredictorTimeStepSize(),1e-12);
+    }
+#endif
   }
 }
 
