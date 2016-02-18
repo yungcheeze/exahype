01f33ac0b06e2662685cc35a9ccfa5a4e5a19853
di25cox <dominic.e.charrier@durham.ac.uk>
2016-02-18 12:30:17 +0000

    Misc/scaling: Added scripts to automate the execution of ExaHyPE applications for a range of core counts.

---
+++ Code/Miscellaneous/JobScripts/scaling/config_eulerflow2d.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# General config file. This file is included by the specialised config files.
+export EXAHYPE_INTERACTIVE=1
+export EXAHYPE_PROJECT_DIR=eulerflow2d
+export EXAHYPE_EXECUTABLE=$EXAHYPE_PROJECT_DIR/ExaHyPE-Euler2d
+export EXAHYPE_OUTPUT_DIR=$EXAHYPE_PROJECT_DIR/scaling
+export EXAHYPE_SPEC_FILE=eulerflow2d.exahype
