090747791fb40dd767336f95aaf368b73ae22fb0
tobiasweinzierl <tobias.weinzierl@durham.ac.uk>
2016-04-23 08:24:44 +0100

    Added. Perhaps forgot this one that is required for MPI.

---
+++ Code/ExaHyPE/exahype/repositories/RepositoryExplicitGridTemplateInstantiation4PredictorRerun.cpp
@@ -0,0 +1,36 @@
+#include "exahype/repositories/Repository.h"
+#include "exahype/records/RepositoryState.h"
+
+#include "exahype/State.h"
+#include "exahype/Vertex.h"
+#include "exahype/Cell.h"
+
+#include "peano/grid/Grid.h"
+
+#include "peano/stacks/CellArrayStack.h"
+#include "peano/stacks/CellSTDStack.h"
+
+#include "peano/stacks/VertexArrayStack.h"
+#include "peano/stacks/VertexSTDStack.h"
+
+ #include "exahype/adapters/InitialGrid.h" 
+ #include "exahype/adapters/PatchInitialisation.h" 
+ #include "exahype/adapters/Plot.h" 
+ #include "exahype/adapters/SolutionUpdateAndGlobalTimeStepComputation.h" 
+ #include "exahype/adapters/PredictorAndGlobalTimeStepComputation.h" 
+ #include "exahype/adapters/ADERDGTimeStep.h" 
+ #include "exahype/adapters/GlobalTimeStepComputation.h" 
+ #include "exahype/adapters/FaceDataExchange.h" 
+ #include "exahype/adapters/Predictor.h" 
+ #include "exahype/adapters/PredictorRerun.h" 
+ #include "exahype/adapters/Corrector.h" 
+
+
+namespace peano {
+  namespace grid {
+    template class Grid<exahype::Vertex,exahype::Cell,exahype::State, peano::stacks::VertexArrayStack<exahype::Vertex> ,peano::stacks::CellArrayStack<exahype::Cell> ,exahype::adapters::PredictorRerun>;
+    template class Grid<exahype::Vertex,exahype::Cell,exahype::State, peano::stacks::VertexSTDStack<  exahype::Vertex> ,peano::stacks::CellSTDStack<  exahype::Cell> ,exahype::adapters::PredictorRerun>;
+  }
+}
+
+#include "peano/grid/Grid.cpph"
