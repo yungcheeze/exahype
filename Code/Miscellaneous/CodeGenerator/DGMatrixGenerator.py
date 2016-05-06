12277f3c866db393e076a41722b522ff1d41c1fe
Angelika Schwarz <angelika.schwarz@tum.de>
2016-05-06 07:46:37 +0200

    CodeGen: Padding for system matrices
     * The CodeGen uses the system matrix always as the left matrix
       in a matrix-matrix multiply -> better for performance
     * The system matrices are not necessarily quadratic, but
       rectangular due to padding
     * The left matrix of a matrix-matrix multiply benefits from
       alignment in Intel's libxsmm. Therefore the matrices receive
       this attribute.

---
+++ Code/Miscellaneous/CodeGenerator/DGMatrixGenerator.py
@@ -0,0 +1,93 @@
+import numpy as np
+import Backend
+import re
+import sys
+sys.path.insert(0, '../aderdg')
+import aderdg
+
+class DGMatrixGenerator:
+    m_config = {}
+
+    # order of the approximation polynomial
+    m_order      = -1
+
+    # number of degrees of freedom
+    m_nDof       = -1
+
+    # number of dimensions we simulate
+    m_nDim       = -1
+
+    # quadrature nodes and weights mapped onto [0,1]
+    m_xGPN       = []
+    m_wGPN       = []
+
+    # SP, DP
+    m_precision  = ''
+
+    # name of generated output file
+    m_filename = "DGMatrices.h"
+
+
+    def __init__(self, i_config, i_precision):
+        self.m_order     = i_config['nDof']-1
+        self.m_config    = i_config
+        self.m_nDim      = i_config['nDim']
+        self.m_precision = i_precision
+
+        # compute the Gauss-Legendre weights
+        x, w = np.polynomial.legendre.leggauss(self.m_order+1)
+        # map onto [0,1]
+        self.m_xGPN = 0.5*(x+1)
+        self.m_wGPN = 0.5*w
+
+
+    def generateCode(self):
+        self.__writeIncludeGuard()
+
+        # padding of the system matrices is always the same
+        l_padHeight = Backend.getPadWidth(self.m_config['nDof'])
+        l_padMatrix = np.zeros((l_padHeight, self.m_config['nDof']))
+
+        # Kxi
+        Kxi = aderdg.assembleStiffnessMatrix(self.m_xGPN, self.m_wGPN, self.m_order)
+        l_linearisedKxi = np.concatenate((Kxi,l_padMatrix),axis=0).flatten('F')
+        # Kxi is now
+        # [ Kxi ]
+        # [0...0]
+        # [0...0]
+        # stored in column major order
+        self.__writeToFile("Kxi", l_linearisedKxi)
+
+        # iK1
+        iK1 = np.linalg.inv(aderdg.assembleK1(Kxi, self.m_xGPN, self.m_order))
+        l_linearisediK1 = np.concatenate((Kxi,l_padMatrix),axis=0).flatten('F')
+        self.__writeToFile("iK1", l_linearisediK1)
+
+        # FLCoeff; FRCoeff
+
+        self.__closeIncludeGuard()
+
+
+    def __writeToFile(self, i_matrixName, i_matrix):
+        l_sourceFile = open(self.m_filename, 'a')
+        if(self.m_precision=='SP'):
+            l_sourceFile.write("const float " + i_matrixName + " __attribute__((aligned(ALIGNMENT))) = {\n")
+        else:
+            l_sourceFile.write("const double " + i_matrixName + " __attribute__((aligned(ALIGNMENT))) = {\n")
+        l_sourceFile.write(',\n'.join(map(str, i_matrix)))
+        l_sourceFile.write("};\n\n")
+        l_sourceFile.close()
+
+
+    def __writeIncludeGuard(self):
+        l_sourceFile = open(self.m_filename, 'a')
+        l_includeGuard = '#ifndef _EXAHYPE_KERNELS_ADERDG_OPTIMISED_DGMATRICES_H_\n'   \
+                         '#define _EXAHYPE_KERNELS_ADERDG_OPTIMISED_DGMATRICES_H_\n\n'
+        l_sourceFile.write(l_includeGuard)
+        l_sourceFile.close()
+
+
+    def __closeIncludeGuard(self):
+        l_sourceFile = open(self.m_filename, 'a')
+        l_sourceFile.write('#endif /* _EXAHYPE_KERNELS_ADERDG_OPTIMISED_DGMATRICES_H_ */')
+        l_sourceFile.close()
