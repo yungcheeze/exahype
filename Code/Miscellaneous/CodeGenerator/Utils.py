6d7a1530d0c6ca77f2f3fe6910de5e2bf931bd09
Angelika Schwarz <angelika.schwarz@tum.de>
2016-06-17 08:37:42 +0200

    CodeGen: Simplify scatter interface
    Working towards an easier interface. Intermediate step.

--- Code/Miscellaneous/CodeGenerator/Utils.py
+++ Code/Miscellaneous/CodeGenerator/Utils.py
@@ -18,10 +18,12 @@ def generateDSCAL(i_scalarName: str, i_inVectorName: str, i_outVectorName:str, i
     return l_code
 
 
-def generateScatter(i_architecture: str, i_nVar: int, i_chunkSize: int) -> str:
-    l_simdWidth = Backend.m_simdWidth['DP'][i_architecture]
-    l_iters = int(i_nVar/l_simdWidth)
-    l_remainder = i_nVar % l_simdWidth
+
+def generateScatter(i_nVar: int, i_chunkSize: int) -> str:
+    i_architecture = Backend.m_architecture
+    i_simdWidth = Backend.m_simdWidth['DP'][i_architecture]
+    l_iters = int(i_nVar/i_simdWidth)
+    l_remainder = i_nVar % i_simdWidth
     l_offset = Backend.getSizeWithPadding(i_nVar)
     # E.g. 9 Vars => 2*simdWidth + 1 => 2*packed + 1*remainder
 
@@ -32,11 +34,11 @@ def generateScatter(i_architecture: str, i_nVar: int, i_chunkSize: int) -> str:
         l_code = '__m256d v1, v2, v3, v4, perm1, perm2, perm3, perm4, res1, res2, res3, res4;\n'
         for it in range(0, l_iters):
             # aligned load
-            l_code = l_code + 'v1 = _mm256_load_pd(&in_buf['+str(0*l_offset+l_simdWidth*it)+']);\n'
-            l_code = l_code + 'v2 = _mm256_load_pd(&in_buf['+str(1*l_offset+l_simdWidth*it)+']);\n'
-            l_code = l_code + 'v3 = _mm256_load_pd(&in_buf['+str(2*l_offset+l_simdWidth*it)+']);\n'
-            l_code = l_code + 'v4 = _mm256_load_pd(&in_buf['+str(3*l_offset+l_simdWidth*it)+']);\n'
-            # vunpckhpd
+            l_code = l_code + 'v1 = _mm256_load_pd(&in_buf['+str(0*l_offset+i_simdWidth*it)+']);\n'
+            l_code = l_code + 'v2 = _mm256_load_pd(&in_buf['+str(1*l_offset+i_simdWidth*it)+']);\n'
+            l_code = l_code + 'v3 = _mm256_load_pd(&in_buf['+str(2*l_offset+i_simdWidth*it)+']);\n'
+            l_code = l_code + 'v4 = _mm256_load_pd(&in_buf['+str(3*l_offset+i_simdWidth*it)+']);\n'
+            # vunpckhpd, vunpcklpd
             l_code = l_code + 'perm1 = _mm256_unpacklo_pd(v1,v2);\n'
             l_code = l_code + 'perm2 = _mm256_unpackhi_pd(v1,v2);\n'
             l_code = l_code + 'perm3 = _mm256_unpacklo_pd(v3,v4);\n'
@@ -59,12 +61,12 @@ def generateScatter(i_architecture: str, i_nVar: int, i_chunkSize: int) -> str:
         # we need some scalar instructions for the remaining variables
         if(l_remainder > 0):
             # aligned packed load - in_buf is padded
-            l_startRemainder = l_iters * l_simdWidth
+            l_startRemainder = l_iters * i_simdWidth
             l_code = l_code + 'v1 = _mm256_load_pd(&in_buf['+str(0*l_offset+l_startRemainder)+']);\n'
             l_code = l_code + 'v2 = _mm256_load_pd(&in_buf['+str(1*l_offset+l_startRemainder)+']);\n'
             l_code = l_code + 'v3 = _mm256_load_pd(&in_buf['+str(2*l_offset+l_startRemainder)+']);\n'
             l_code = l_code + 'v4 = _mm256_load_pd(&in_buf['+str(3*l_offset+l_startRemainder)+']);\n'
-            # vunpckhpd
+            # vunpckhpd, vunpcklpd
             l_code = l_code + 'perm1 = _mm256_unpacklo_pd(v1,v2);\n'
             l_code = l_code + 'perm2 = _mm256_unpackhi_pd(v1,v2);\n'
             l_code = l_code + 'perm3 = _mm256_unpacklo_pd(v3,v4);\n'
@@ -88,8 +90,8 @@ def generateScatter(i_architecture: str, i_nVar: int, i_chunkSize: int) -> str:
         l_code = '__m128d v1,v2,res;\n'
         for it in range(0, l_iters):
             # aligned load
-            l_code = l_code + 'v1 = _mm_load_pd(&in_buf['+str(0*l_offset+l_simdWidth*it)+']);\n'
-            l_code = l_code + 'v2 = _mm_load_pd(&in_buf['+str(1*l_offset+l_simdWidth*it)+']);\n'
+            l_code = l_code + 'v1 = _mm_load_pd(&in_buf['+str(0*l_offset+i_simdWidth*it)+']);\n'
+            l_code = l_code + 'v2 = _mm_load_pd(&in_buf['+str(1*l_offset+i_simdWidth*it)+']);\n'
             # unpcklpd
             l_code = l_code + 'res = _mm_unpacklo_pd(v1, v2);\n'
             # unaligned store. Should later on become _mm_store_pd()
@@ -102,7 +104,7 @@ def generateScatter(i_architecture: str, i_nVar: int, i_chunkSize: int) -> str:
 
         # there is one variable remaining
         if(l_remainder > 0):
-            l_startRemainder = l_iters * l_simdWidth
+            l_startRemainder = l_iters * i_simdWidth
             l_code = l_code + 'out_buf['+str(l_startAddress)+'] = in_buf['+str(l_startRemainder)+'];\n'
             l_code = l_code + 'out_buf['+str(l_startAddress+1)+'] = in_buf['+str(l_startRemainder+l_offset)+'];\n'
 
@@ -110,5 +112,4 @@ def generateScatter(i_architecture: str, i_nVar: int, i_chunkSize: int) -> str:
         for iVar in range(0, i_nVar):
             l_code = l_code + 'out_buf['+str(iVar*i_chunkSize)+'] = in_buf['+str(iVar)+'];\n'
 
-    print(l_code)
     return l_code
