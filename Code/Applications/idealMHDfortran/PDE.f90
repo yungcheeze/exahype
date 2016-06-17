dc80b1b080ec3bb4726e587b7a5bde3a15012c28
Sven Köppel <koeppel@fias.uni-frankfurt.de>
2016-06-17 17:07:35 +0200

    idealMHDfortran: Added the new Application. However, it is not finished and therefore also not yet tested (likely not to compile). We are missing the PDE source terms, this problem was raised 3 months ago in #21.
    Also, there will be a lot of redundancy as I have to copy 90% of the srhd3dfortran code for the idealMHDfortran application. Instead, this really should be one single application where one can choose about what one wants to compile in the exahype specification file or by environment variables on compile time.

---
+++ Code/Applications/idealMHDfortran/PDE.f90
@@ -0,0 +1,164 @@
+!!!
+!!! A Special Relativistic ideal Magnetohydrodynamics ExaHyPE Kernel.
+!!! Based on files provided by Olindo, adopted by Sven.
+!!! Based on srhd3dfortran/PDE.f90.
+!!!
+
+MODULE eos
+  ! Equation of State parameters.
+  ! Currently only an ideal EOS is implemented.
+  IMPLICIT NONE
+  PUBLIC
+  REAL, PARAMETER                :: gamma = 5.0/3.0
+ENDMODULE
+
+SUBROUTINE PDEEigenvalues(Lambda,Q,nv) 
+  USE typesDef, ONLY : nVar, d 
+  USE eos, ONLY : gamma
+  USE, INTRINSIC :: ISO_C_BINDING 
+  IMPLICIT NONE 
+  ! Argument list  
+  REAL, INTENT(IN)  :: Q(nVar), nv(d)  
+  REAL, INTENT(OUT) :: Lambda(nVar)  
+  ! Local variables  
+  INTEGER :: iErr
+  REAL :: rho,vx,vy,vz,p
+  REAL :: cs2, cs, c0, v2, w, gamma1, vn, den, u, c
+  REAL :: ex, ey, ez, b2, e2, cs2, ca2, vf1, vf2
+  REAL :: V(nVar)
+
+  ! These are not the exact eigenvalues, instead of Lambda(1..9)
+  ! we compute only two eigenvalues: Approximate magnetosonics
+
+  CALL PDECons2Prim(V,Q,xg,ltime,iErr)
+  rho    = V(1)
+  vx     = V(2)
+  vy     = V(3)
+  vz     = V(4)
+  p      = V(5)
+  bx     = V(6)
+  by     = V(7)
+  bz     = V(8)
+
+  ex     = - (vy*bz - vz*by)
+  ey     = - (vz*bx - vx*bz)
+  ez     = - (vx*by - vy*bx)
+  gamma1 = gamma/(gamma-1.0)
+  b2     = bx*bx + by*by + bz*bz
+  v2     = vx*vx + vy*vy + vz*vz
+  e2     = ex*ex + ey*ey + ez*ez
+  w      = rho + gamma1*p
+  vn     = vx*n(1) + vy*n(2) + vz*n(3)
+
+  cs2    = EQN%gamma * p / w
+  ca2    = (b2 - e2) / ( w + b2 - e2)
+  a2     = cs2 + ca2 - cs2 * ca2
+  den    = 1.0/( 1.0 - v2 * a2)
+  vf1    = den * ( 1.0 - a2) * vn
+  vf2    = den * sqrt(a2 * ( 1.0 - v2) * ((1.0 - v2 * a2) - (1.0 - a2) * vn**2))
+
+  Lambda(1) = vf1 + vf2
+  Lambda(2) = vf1 - vf2
+  Lambda(3) = 0.0
+  Lambda(4) = 0.0
+  Lambda(5) = 0.0
+  Lambda(6) = 0.0
+  Lambda(7) = 0.0
+  Lambda(8) = 0.0
+  Lambda(9) = 0.0
+
+END SUBROUTINE PDEEigenvalues
+ 
+
+
+SUBROUTINE PDEFlux(F,Q) 
+  USE typesDef, ONLY : nVar, d 
+  USE eos, ONLY : gamma
+  USE, INTRINSIC :: ISO_C_BINDING 
+  IMPLICIT NONE 
+  ! Argument list  
+  REAL, INTENT(IN)  :: Q(nVar) 
+  REAL, INTENT(OUT) :: F(nVar,d) 
+  ! Local variables  
+  REAL :: rho,vx,vy,vz,p
+  REAL :: v2,lf,w,ww,wwx,wwy,wwz,gamma1
+  REAL :: ex,ey,ez,v2,b2,e2,uem
+  REAL :: V(nVar)
+  INTEGER :: iErr
+
+  CALL PDECons2Prim(V,Q,iErr)
+  gamma1 = gamma/(gamma-1.0)
+  rho    = V(1)
+  vx     = V(2)
+  vy     = V(3)
+  vz     = V(4)
+  p      = V(5)
+  bx     = V(6)
+  by     = V(7)
+  bz     = V(8)
+
+  ex     = - (vy*bz - vz*by)
+  ey     = - (vz*bx - vx*bz)
+  ez     = - (vx*by - vy*bx)
+
+  v2     = vx**2 + vy**2 + vz**2
+  b2     = bx**2 + by**2 + bz**2
+  e2     = ex**2 + ey**2 + ez**2
+  lf     = 1.0/sqrt(1.0 - v2)
+  w      = rho + gamma1*p
+  ww     = w*lf**2
+  uem    = 0.5*(b2 + e2)
+  wwx    = ww*vx
+  wwy    = ww*vy
+  wwz    = ww*vz
+
+  !
+  F(1, 1) = vx*rho*lf
+  F(2, 1) = wwx*vx - bx*bx - ex*ex + p + uem
+  F(3, 1) = wwx*vy - bx*by - ex*ey
+  F(4, 1) = wwx*vz - bx*bz - ex*ez 
+  F(5, 1) = wwx + (ey*bz - ez*by) 
+  F(6, 1) = V(9)
+  F(7, 1) = -ez
+  F(8, 1) = ey  
+  F(9, 1) = DivCleaning_a**2*bx
+  !
+  F(1, 2) = vy*rho*lf
+  F(2, 2) = wwy*vx - by*bx - ey*ex 
+  F(3, 2) = wwy*vy - by*by - ey*ey + p + uem
+  F(4, 2) = wwy*vz - by*bz - ey*ez 
+  F(5, 2) = wwy + (ez*bx - ex*bz) 
+  F(6, 2) = ez 
+  F(7, 2) = V(9) 
+  F(8, 2) = -ex   
+  F(9, 2) = DivCleaning_a**2*by
+  !
+  F(1, 3) = vz*rho*lf
+  F(2, 3) = wwz*vx - bz*bx - ez*ex 
+  F(3, 3) = wwz*vy - bz*by - ez*ey 
+  F(4, 3) = wwz*vz - bz*bz - ez*ez + p + uem
+  F(5, 3) = wwz + (ex*by - ey*bx) 
+  F(6, 3) = -ey  
+  F(7, 3) = ex   
+  F(8, 3) = V(9)   
+  F(9, 3) = DivCleaning_a**2*bz
+  !
+END SUBROUTINE PDEFlux 
+ 
+
+
+SUBROUTINE PDENCP(BgradQ,Q,gradQ) 
+  ! not used  
+  PRINT *, 'PDENCP is not used for nonlinear-solvers' 
+  CALL EXIT  
+  !
+END SUBROUTINE PDENCP 
+ 
+
+
+SUBROUTINE PDEMatrixB(Bn,Q,nv) 
+  ! not used  
+  PRINT *, 'PDEMatrixB is not used for nonlinear-solvers' 
+  CALL EXIT  
+  !
+END SUBROUTINE PDEMatrixB 
---
+++ Code/Applications/idealMHDfortran/idealMHDSolver.cpp
@@ -0,0 +1,69 @@
+#include "idealMHDSolver.h"
+
+#include <memory>
+
+srhd3dfortran::idealMHDSolver::idealMHDSolver( int kernelNumber, std::unique_ptr<exahype::profilers::Profiler> profiler):
+  exahype::solvers::Solver("idealMHDSolver",exahype::solvers::Solver::ADER_DG,kernelNumber,9,3+1,exahype::solvers::Solver::GlobalTimeStepping, std::move(profiler)) {
+  // @todo Please implement/augment if required
+}
+
+
+
+int srhd3dfortran::idealMHDSolver::getMinimumTreeDepth() const {
+  // @todo Please implement
+  return 3;
+}
+
+
+
+bool srhd3dfortran::idealMHDSolver::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double> &center, const tarch::la::Vector<DIMENSIONS, double> &dx, double t) {
+  // @todo Please implement
+  return false;
+}
+
+
+
+void srhd3dfortran::idealMHDSolver::adjustedSolutionValues(const double* const x,const double w,const double t,const double dt,double* Q) {
+	// Dimensions             = 3
+	// Number of variables    = 9 (#unknowns + #parameters)
+
+	if (tarch::la::equals(t, 0.0)) {
+		double V[9];
+		
+		// specifiy initial values for V[0]...V[8] here.
+		
+		// prim2cons(V, Q);
+	}
+	
+	// why does the toolkit propose this?:
+
+	Q[0] = 0.0;
+	Q[1] = 0.0;
+	Q[2] = 0.0;
+	Q[3] = 0.0;
+	Q[4] = 0.0;
+	Q[5] = 0.0;
+	Q[6] = 0.0;
+	Q[7] = 0.0;
+	Q[8] = 0.0;
+}
+
+
+
+exahype::solvers::Solver::RefinementControl srhd3dfortran::idealMHDSolver::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t, const int level) {
+  // @todo Please implement
+  return exahype::solvers::Solver::Keep;
+}
+
+
+
+
+
+
+//************************************************* 
+//for FORTRAN kernels the fluxes and eigenvalues 
+//have to be implemented in the file ./PDE.f90 
+//************************************************* 
+
+
+
