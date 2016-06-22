5fb32c82674742a6d337d7050e968366fb0991a4
Fabian Guera <fabian.guera@gmail.com>
2016-06-22 18:00:54 +0200

    FIX LikwidPowerAndEnergyMonitoringModule

--- Code/ExaHyPE/exahype/profilers/likwid/modules/LikwidPowerAndEnergyMonitoringModule.cpp
+++ Code/ExaHyPE/exahype/profilers/likwid/modules/LikwidPowerAndEnergyMonitoringModule.cpp
@@ -3,14 +3,14 @@
  * Copyright (c) 2016  http://exahype.eu
  * All rights reserved.
  *
- * The project has received funding from the European Union's Horizon 
+ * The project has received funding from the European Union's Horizon
  * 2020 research and innovation programme under grant agreement
  * No 671698. For copyrights and licensing, please consult the webpage.
  *
  * Released under the BSD 3 Open Source License.
  * For the full license text, see LICENSE.txt
  **/
- 
+
 #include "LikwidPowerAndEnergyMonitoringModule.h"
 
 #ifdef LIKWID_AVAILABLE
@@ -108,7 +108,7 @@ void LikwidPowerAndEnergyMonitoringModule::start(const std::string& tag) {
     for (int j = 0; j < kNumberOfProfiledPowerTypes; j++) {
       // start power measurement
       power_start(&vec_array_power_data[i][j], cpuid_of_sockets_[i],
-                  kProfiledPowerTypes[i]);
+                  kProfiledPowerTypes[j]);
     }
   }
 }
