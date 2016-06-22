bcea67fe23698480f805dbad8f790a0437bdb01e
vvarduhn <vvarduhn@umn.edu>
2016-06-22 11:11:29 +0200

    ADD ExaHyPE license

--- Code/ExaHyPE/exahype/profilers/ipcm/metrics/IpcmConsumedJoulesMetric.h
+++ Code/ExaHyPE/exahype/profilers/ipcm/metrics/IpcmConsumedJoulesMetric.h
@@ -1,3 +1,16 @@
+/**
+ * This file is part of the ExaHyPE project.
+ * Copyright (c) 2016  http://exahype.eu
+ * All rights reserved.
+ *
+ * The project has received funding from the European Union's Horizon 
+ * 2020 research and innovation programme under grant agreement
+ * No 671698. For copyrights and licensing, please consult the webpage.
+ *
+ * Released unter the BSD 3 Open Source License.
+ * For the full license text, see LICENSE.txt
+ **/
+ 
 #ifndef _EXAHYPE_PROFILERS_IPCM_METRICS_IPCM_CONSUMED_JOULES_METRIC_H_
 #define _EXAHYPE_PROFILERS_IPCM_METRICS_IPCM_CONSUMED_JOULES_METRIC_H_
 
--- Code/ExaHyPE/kernels/aderdg/generic/fortran/3d/spaceTimePredictorNonlinearFortran.f90
+++ Code/ExaHyPE/kernels/aderdg/generic/fortran/3d/spaceTimePredictorNonlinearFortran.f90
@@ -1,3 +1,16 @@
+/**
+ * This file is part of the ExaHyPE project.
+ * Copyright (c) 2016  http://exahype.eu
+ * All rights reserved.
+ *
+ * The project has received funding from the European Union's Horizon 
+ * 2020 research and innovation programme under grant agreement
+ * No 671698. For copyrights and licensing, please consult the webpage.
+ *
+ * Released unter the BSD 3 Open Source License.
+ * For the full license text, see LICENSE.txt
+ **/
+ 
    
     
 SUBROUTINE ADERPicardLoopNonlinear(luh,dt,dx,lqh,lFh) 
