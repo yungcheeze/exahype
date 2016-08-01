56ce9e5b01f4f17742fa088b5576a158090e177a
Vasco Varduhn <varduhn@tum.de>
2016-08-01 12:28:33 +0200

    MOD added option for Intel compilers to job scripts for scalability tests

--- Code/Miscellaneous/JobScripts/SLURM.scaling.py
+++ Code/Miscellaneous/JobScripts/SLURM.scaling.py
@@ -10,7 +10,7 @@ from SLURM import runMultipleSlURMjobs
 processes = [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 1680]
 pdegrees = [1, 2, 3, 4, 5, 6, 7, 8]
 hmaxs = ["0.04", "0.02", "0.005"]
-compilers = ["GNU"]
+compilers = ["GNU", "Intel"]
 dimensions = ["2D", "3D"]
 modes = ["Release"]
 
