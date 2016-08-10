ce24b505e6023f142a788d02a53c0d8dec8199a3
Fabian Guera <fabian.guera@gmail.com>
2016-08-10 15:47:44 +0200

    Toolkit: Write correct FVM signatures

--- Code/Toolkit/src/eu/exahype/solvers/GenericFiniteVolumesMUSCLinC.java
+++ Code/Toolkit/src/eu/exahype/solvers/GenericFiniteVolumesMUSCLinC.java
@@ -9,12 +9,13 @@ public class GenericFiniteVolumesMUSCLinC implements Solver {
   private boolean _enableProfiler;
   private boolean _hasConstants;
 
-  public GenericFiniteVolumesMUSCLinC(int numberOfVariables, int numberOfParameters, int patchSize, boolean enableProfiler, boolean hasConstants) {
-    _numberOfVariables  = numberOfVariables;
+  public GenericFiniteVolumesMUSCLinC(int numberOfVariables, int numberOfParameters, int patchSize,
+      boolean enableProfiler, boolean hasConstants) {
+    _numberOfVariables = numberOfVariables;
     _numberOfParameters = numberOfParameters;
-    _patchSize          = patchSize;
-    _enableProfiler     = enableProfiler;
-    _hasConstants       = hasConstants;
+    _patchSize = patchSize;
+    _enableProfiler = enableProfiler;
+    _hasConstants = hasConstants;
   }
 
   public void writeHeader(java.io.BufferedWriter writer, String solverName, String projectName)
@@ -22,7 +23,8 @@ public class GenericFiniteVolumesMUSCLinC implements Solver {
     Helpers.writeMinimalFiniteVolumesSolverHeader(solverName, writer, projectName, _hasConstants);
 
     writer.write("  private:\n");
-    writer.write("    static void eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda);\n");
+    writer.write(
+        "    static void eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda);\n");
     writer.write("    static void flux(const double* const Q, double** F);\n");
     writer.write("    static void source(const double* const Q, double* S);\n");
     writer.write("};\n\n\n");
@@ -36,38 +38,37 @@ public class GenericFiniteVolumesMUSCLinC implements Solver {
     writer.write("#include \"" + solverName + ".h\"\n");
     writer.write("#include \"kernels/finitevolumes/muscl/c/2d/solutionUpdate.cpph\"\n");
     writer.write("#include \"kernels/finitevolumes/muscl/c/3d/solutionUpdate.cpph\"\n");
+    writer.write("#include \"kernels/finitevolumes/muscl/c/2d/stableTimeStepSize.cpph\"\n");
+    writer.write("#include \"kernels/finitevolumes/muscl/c/3d/stableTimeStepSize.cpph\"\n");
     writer.write("\n\n\n");
 
-    writer.write("double " + projectName + "::" + solverName + "::stableTimeStepSize( double* luh[THREE_POWER_D], const tarch::la::Vector<DIMENSIONS, double>& dx) {\n" );
+    writer.write("double " + projectName + "::" + solverName
+        + "::stableTimeStepSize( double* luh[THREE_POWER_D], const tarch::la::Vector<DIMENSIONS, double>& dx) {\n");
     if (_enableProfiler) {
-        writer.write("  _profiler->start(\"solutionUpdate\");\n");
-      }
-      writer.write( 
-        "  double maxAdmissibleDt=0;\n"
-      );
-      writer.write(
-        "  kernels::finitevolumes::muscl::c::solutionUpdate<flux,eigenvalues>( luh, dx, std::numeric_limits<double>::max(), getNumberOfVariables(), getNodesPerCoordinateAxis(), maxAdmissibleDt );\n"
-      );
-      if (_enableProfiler) {
-        writer.write("  _profiler->stop(\"solutionUpdate\");\n");
-      }
-      writer.write( 
-        "  return maxAdmissibleDt;\n"
-      );
-    writer.write("}\n\n\n" );
+      writer.write("  _profiler->start(\"stableTimeStepSize\");\n");
+    }
+    writer.write(
+        "  double maxAdmissibleDt = kernels::finitevolumes::muscl::c::stableTimeStepSize<eigenvalues>(luh, dx, getNumberOfVariables(), "
+            + _patchSize + " /* patchSize */);\n");
+    if (_enableProfiler) {
+      writer.write("  _profiler->stop(\"stableTimeStepSize\");\n");
+    }
+    writer.write("  return maxAdmissibleDt;\n");
+    writer.write("}\n\n\n");
 
-    writer.write("void " + projectName + "::" + solverName + "::solutionUpdate(double* luh[THREE_POWER_D], const tarch::la::Vector<DIMENSIONS, double>& dx, const double dt, double& maxAdmissibleDt) {\n" );
+    writer.write("void " + projectName + "::" + solverName
+        + "::solutionUpdate(double* luh[THREE_POWER_D], const tarch::la::Vector<DIMENSIONS, double>& dx, const double dt, double& maxAdmissibleDt) {\n");
     if (_enableProfiler) {
       writer.write("  _profiler->start(\"solutionUpdate\");\n");
     }
     writer.write(
-      "  kernels::finitevolumes::muscl::c::solutionUpdate<flux,eigenvalues>( luh, dx, dt, getNumberOfVariables(), getNodesPerCoordinateAxis(), maxAdmissibleDt );\n"
-    );
+        "  kernels::finitevolumes::muscl::c::solutionUpdate<flux,source,eigenvalues>(luh, dx, dt, getNumberOfVariables(), "
+            + _patchSize + " /* patchSize */);\n");
     if (_enableProfiler) {
       writer.write("  _profiler->stop(\"solutionUpdate\");\n");
     }
-    writer.write("}\n\n\n" );
-    
+    writer.write("}\n\n\n");
+
     writer.write("\n\n\n");
   }
 
@@ -75,28 +76,32 @@ public class GenericFiniteVolumesMUSCLinC implements Solver {
       String projectName) throws java.io.IOException {
     writer.write("#include \"" + solverName + ".h\"\n");
     writer.write("\n\n\n");
-    
+
     if (_hasConstants) {
-      writer.write(projectName + "::" + solverName + "::" + solverName + "(int cellsPerCoordinateAxis, double maximumMeshSize, exahype::solvers::Solver::TimeStepping timeStepping, std::unique_ptr<exahype::profilers::Profiler> profiler, exahype::Parser::ParserView constants):\n");
+      writer.write(projectName + "::" + solverName + "::" + solverName
+          + "(int cellsPerCoordinateAxis, double maximumMeshSize, exahype::solvers::Solver::TimeStepping timeStepping, std::unique_ptr<exahype::profilers::Profiler> profiler, exahype::Parser::ParserView constants):\n");
+    } else {
+      writer.write(projectName + "::" + solverName + "::" + solverName
+          + "(int cellsPerCoordinateAxis, double maximumMeshSize, exahype::solvers::Solver::TimeStepping timeStepping, std::unique_ptr<exahype::profilers::Profiler> profiler):\n");
     }
-    else {
-      writer.write(projectName + "::" + solverName + "::" + solverName + "(int cellsPerCoordinateAxis, double maximumMeshSize, exahype::solvers::Solver::TimeStepping timeStepping, std::unique_ptr<exahype::profilers::Profiler> profiler):\n");
-    }
-    
-    writer.write("  exahype::solvers::FiniteVolumesSolver("
-            + "\""+solverName+"\", "+_numberOfVariables+", "+_numberOfParameters+", cellsPerCoordinateAxis, maximumMeshSize, timeStepping, std::move(profiler)) {\n");
+
+    writer.write("  exahype::solvers::FiniteVolumesSolver(" + "\"" + solverName + "\", "
+        + _numberOfVariables + ", " + _numberOfParameters
+        + ", cellsPerCoordinateAxis, maximumMeshSize, timeStepping, std::move(profiler)) {\n");
     writer.write("  // @todo Please implement/augment if required\n");
     writer.write("}\n");
     writer.write("\n\n\n");
 
-    writer.write("void " + projectName + "::" + solverName + "::solutionAdjustment( double* luh, const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t, double dt) {\n" );
+    writer.write("void " + projectName + "::" + solverName
+        + "::solutionAdjustment( double* luh, const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t, double dt) {\n");
     writer.write("  if ( tarch::la::equals(t,0.0) ) {\n");
     writer.write("  // @todo Please implement and set initial conditions\n");
     writer.write("  }\n");
     writer.write("  // @todo Feel free to add further conditions\n");
-    writer.write("}\n\n\n" );
+    writer.write("}\n\n\n");
 
-    writer.write("bool " + projectName + "::" + solverName + "::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t) {\n" );
+    writer.write("bool " + projectName + "::" + solverName
+        + "::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t) {\n");
     writer.write("  // @todo Please implement\n");
     writer.write("  if ( tarch::la::equals(t,0.0) ) {\n");
     writer.write("    // Tell kernel that you want to set initial conditions \n");
@@ -106,30 +111,38 @@ public class GenericFiniteVolumesMUSCLinC implements Solver {
     writer.write("    // @todo Please implement\n");
     writer.write("    return false; \n");
     writer.write("  }\n");
-    writer.write("}\n\n\n" );
+    writer.write("}\n\n\n");
 
-    writer.write("exahype::solvers::Solver::RefinementControl " + projectName + "::" + solverName + "::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center,const tarch::la::Vector<DIMENSIONS, double>& dx, double t,const int level) {\n" );
+    writer.write("exahype::solvers::Solver::RefinementControl " + projectName + "::" + solverName
+        + "::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center,const tarch::la::Vector<DIMENSIONS, double>& dx, double t,const int level) {\n");
     writer.write("  // @todo Please implement\n");
-    writer.write("  return exahype::solvers::Solver::RefinementControl::Keep;\n" );
-    writer.write("}\n\n\n" );
+    writer.write("  return exahype::solvers::Solver::RefinementControl::Keep;\n");
+    writer.write("}\n\n\n");
 
-    writer.write("void " + projectName + "::" + solverName + "::eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda) {\n" );
+    writer.write("void " + projectName + "::" + solverName
+        + "::eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda) {\n");
     writer.write("  // @todo Please implement\n");
-    writer.write("}\n\n\n" );
+    writer.write("}\n\n\n");
 
-    writer.write("void " + projectName + "::" + solverName + "::flux(const double* const Q, double** F) {\n" );
+    writer.write("void " + projectName + "::" + solverName
+        + "::flux(const double* const Q, double** F) {\n");
     writer.write("  // @todo Please implement\n");
-    writer.write("}\n\n\n" );
+    writer.write("}\n\n\n");
+    
+    writer.write("void " + projectName + "::" + solverName
+        + "::source(const double* const Q, double* S) {\n");
+    writer.write("  // @todo Please implement\n");
+    writer.write("}\n\n\n");
   }
-  
-  
+
+
   public void writeUserPDE(java.io.BufferedWriter writer, String solverName, String projectName)
       throws java.io.IOException {
     // @todo Implement
     System.err.println("C-style kernels do not have a PDF.f90.\n");
   }
-  
-  
+
+
   public void writeTypesDef(java.io.BufferedWriter writer, String solverName, String projectName)
       throws java.io.IOException {
     // @todo Implement
