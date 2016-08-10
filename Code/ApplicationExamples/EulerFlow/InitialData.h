28278d1f5cd675ad8dfcd0b9b796ba902a916793
Fabian Guera <fabian.guera@gmail.com>
2016-08-10 15:40:04 +0200

    FIX return temporary

--- Code/ApplicationExamples/EulerFlow/InitialData.h
+++ Code/ApplicationExamples/EulerFlow/InitialData.h
@@ -1,20 +1,13 @@
 #ifndef __INITIAL_DATA_EULERFLOW__
 #define __INITIAL_DATA_EULERFLOW__
 
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 #include <string>
 
-/* This is where we get our parameter decision from: */
-inline const std::string& getEnvParameter(const char* key) {
-	if(char* env = std::getenv(key))
-		return env;
-	else return "";
-}
+void InitialData(const double* const x, double* Q, double t);
 
-void InitialData(const double* const  x, double* Q, double t);
-
-void ShuVortex2D(const double* const  x, double* Q, double t);
+void ShuVortex2D(const double* const x, double* Q, double t);
 
 void MovingGauss2D(const double* const x, double* V, double t);
 
