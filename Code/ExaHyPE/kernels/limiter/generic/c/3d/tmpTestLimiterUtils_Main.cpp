13252494b83c8b2a404c7dc572eac02756a6e774
Jean-Matthieu Gallard <ga96nuv3@mpp2-login6.cos.lrz.de>
2016-08-23 16:19:59 +0200

    Limiter - add kernel limiterUtils

---
+++ Code/ExaHyPE/kernels/limiter/generic/c/3d/tmpTestLimiterUtils_Main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#define DIMENSIONS 3 
+#include "limiterUtils.cpph"
+
+void getTestData(double* &luh, int& numberOfVariable, int& basisSize) { 
+  numberOfVariable = 5;
+  basisSize = 4;
+  int luhSize = numberOfVariable*basisSize*basisSize*basisSize;
+  luh = new double[luhSize];
+  for(int i=0; i<luhSize; i++) {
+    luh[i] = i+ 0.5;
+  }
+}
+
+int main() {
+  double* luh;
+  int numberOfVariable, basisSize;
+  getTestData(luh, numberOfVariable, basisSize);
+  
+  double* localMin = new double[numberOfVariable];
+  double* localMax = new double[numberOfVariable];
+  
+  std::cout << "Hello World: " << luh[10] << std::endl;
+  
+  kernels::limiter::generic::c::findCellLocallocalMinlocalMax(luh, numberOfVariable, basisSize, localMin, localMax);
+  
+  std::cout << localMin[4] << std::endl;
+  std::cout << localMax[4] << std::endl;
+  
+  double* troubledMin = new double[numberOfVariable];
+  double* troubledMax = new double[numberOfVariable];
+  
+  double tMin = 0;
+  double tMax = 319.499;
+  std::cout << "troubled cell limit: " << tMin << " - " << tMax <<std::endl;
+  
+  for(int i=0;i<numberOfVariable;i++) {
+    troubledMin[i] = tMin;
+    troubledMax[i] = tMax;
+  }
+  
+  if(kernels::limiter::generic::c::isTroubledCell(luh, numberOfVariable, basisSize, troubledMin, troubledMax))
+  {
+    std::cout << "troubled cell" << std::endl;
+  } 
+  else {
+    std::cout << "not troubled cell" << std::endl;
+  }
+  
+  return 0;
+}
