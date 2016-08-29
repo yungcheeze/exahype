bb99767a9fb2929523775ced16e1b1620fde5567
Jean-Matthieu Gallard <ga96nuv3@mpp2-login6.cos.lrz.de>
2016-08-29 11:09:08 +0200

    Limiter - uh2lim and refactor to match kernel structure

---
+++ Code/ExaHyPE/kernels/limiter/generic/Limiter.h
@@ -0,0 +1,54 @@
+/**
+ * This file is part of the ExaHyPE project.
+ * Copyright (c) 2016  http://exahype.eu
+ * All rights reserved.
+ *
+ * The project has received funding from the European Union's Horizon
+ * 2020 research and innovation programme under grant agreement
+ * No 671698. For copyrights and licensing, please consult the webpage.
+ *
+ * Released under the BSD 3 Open Source License.
+ * For the full license text, see LICENSE.txt
+ **/
+
+#ifndef _EXAHYPE_KERNELS_LIMITER_GENERIC_H_
+#define _EXAHYPE_KERNELS_ADERDG_GENERIC_H_
+
+#include <algorithm>
+#include "../../GaussLegendreQuadrature.h"
+#include "../../KernelUtils.h"
+
+
+namespace kernels {
+namespace limiter {
+namespace generic {
+namespace c {
+  
+  
+extern double* uh2lim;
+extern double* uh2lob;
+extern double* lim2uh;
+
+int getLimBasisSize(const int basisSize);
+
+void BaseFunc1D(double* phi, double xi, const int basisSize);
+
+void initProjectionMatrices(const int basisSize);
+
+void freeProjectionMatrices(const int basisSize);
+
+double* getGaussLobattoData(const double* const luh, const int numberOfVariables, const int basisSize, int& basisSizeLob);
+
+double* getFVMData(const double* const luh, const int numberOfVariables, const int basisSize, int& basisSizeLim);
+
+void findCellLocallocalMinlocalMax(const double* const luh, const int numberOfVariables, const int basisSize, double* localMin, double* localMax);
+
+bool isTroubledCell(const double* const luh, const int numberOfVariables, const int basisSize, const double* const troubledMin, const double* const troubledMax);
+
+} // namespace c
+} // namespace generic
+} // namespace limiter
+} // namespace kernel
+
+
+#endif //_EXAHYPE_KERNELS_LIMITER_GENERIC_H_
--- Code/ExaHyPE/kernels/limiter/generic/c/3d/tmpTestLimiterUtils_Main.cpp
+++ Code/ExaHyPE/kernels/limiter/generic/c/3d/tmpTestLimiterUtils_Main.cpp
@@ -1,7 +1,11 @@
-#include <iostream>
+// g++ -c -DDIMENSIONS=3 limiterUtils.cpp && g++ -c tmpTestLimiterUtils_Main.cpp && g++ -o main tmpTestLimiterUtils_Main.o ../../../../GaussLegendreQuadrature.o limiterUtils.o 
 
-#define DIMENSIONS 3 
-#include "limiterUtils.cpph"
+// 
+#include <iostream>
+#include <iomanip>
+#include <set>
+
+#include "../../Limiter.h"
 
 void getTestData(double* &luh, int& numberOfVariable, int& basisSize) { 
   numberOfVariable = 5;
@@ -14,7 +18,7 @@ void getTestData(double* &luh, int& numberOfVariable, int& basisSize) {
 }
 
 
-/**
+/*
 int main() {
   double* luh;
   int numberOfVariable, basisSize;
@@ -24,6 +28,32 @@ int main() {
   double* localMax = new double[numberOfVariable];
   
   std::cout << "Hello World: " << luh[10] << std::endl;
+  
+  std::set<int> orders;
+  kernels::initGaussLegendreNodesAndWeights(orders);
+  std::cout << "gaussLegendreNodes: ";
+  for(int i=0; i< basisSize; i++)
+    std::cout << kernels::gaussLegendreNodes[basisSize-1][i] << " ";
+  std::cout <<std::endl;
+  
+  //test BaseFunc1D
+  double xi = 0.37;
+  double* phi = new double[basisSize];
+  kernels::limiter::generic::c::BaseFunc1D(phi, xi, basisSize);
+  std::cout << "phi for xi = 0.37: ";
+  for(int i=0; i< basisSize; i++)
+    std::cout << phi[i] << " ";
+  std::cout <<std::endl;
+  
+  kernels::limiter::generic::c::initProjectionMatrices(basisSize);
+  int bLim = 2*(basisSize-1)+1;
+  for(int i=0; i< basisSize; i++) {
+    for(int j=0; j<bLim; j++)
+      std::cout << kernels::limiter::generic::c::uh2lim[i*bLim+j] << " ";
+    std::cout <<std::endl;
+  }
+  std::cout <<std::endl;
+  std::cout << "uh2lim[1][1]: " <<kernels::limiter::generic::c::uh2lim[1*bLim+1] << std::endl;
   
   kernels::limiter::generic::c::findCellLocallocalMinlocalMax(luh, numberOfVariable, basisSize, localMin, localMax);
   
@@ -49,6 +79,13 @@ int main() {
   else {
     std::cout << "not troubled cell" << std::endl;
   }
+  
+  int basisSizeLim = 0;
+  double* lim = kernels::limiter::generic::c::getFVMData(luh, numberOfVariable, basisSize, basisSizeLim);
+ 
+  std::cout << "basisSizeLim: " << basisSizeLim << std::endl;  
+  kernels::idx4 idxLim(basisSizeLim,basisSizeLim,basisSizeLim,numberOfVariable);
+  std::cout << "lim[iVar=0][x=0][y=1][z=3]: " << std::setprecision (15) << lim[idxLim(3,1,0,0)] << std::endl;
   
   return 0;
 }
