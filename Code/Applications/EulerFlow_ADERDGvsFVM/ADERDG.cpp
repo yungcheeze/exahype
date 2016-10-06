3e63976d3c13aa7015fcaffe2a5921cefd999200
Jean-Matthieu Gallard <ga96nuv3@mpp2-login6.cos.lrz.de>
2016-10-06 16:54:51 +0200

    2 Solvers - ADERDG vs FVM for EulerFlow

---
+++ Code/Applications/EulerFlow_ADERDGvsFVM/ADERDG.cpp
@@ -0,0 +1,128 @@
+#include "ADERDG.h"
+
+#include <memory>
+
+void Euler::ADERDG::init() {
+  // This function is called inside the constructur.
+  // @todo Please implement/augment if required.
+}
+
+void Euler::ADERDG::flux(const double* const Q, double** F) {
+  // Dimensions             = 2
+  // Number of variables    = 5 (#unknowns + #parameters)
+  const double GAMMA = 1.4;
+
+  const double irho = 1.0 / Q[0];
+  const double p =
+      (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2]) * irho);
+
+  double* f = F[0];
+  double* g = F[1];
+
+  // @todo Please implement
+  // f
+  f[0] = Q[1];
+  f[1] = irho * Q[1] * Q[1] + p;
+  f[2] = irho * Q[1] * Q[2];
+  f[3] = irho * Q[1] * Q[3];
+  f[4] = irho * Q[1] * (Q[4] + p);
+  // g
+  // @todo Please implement
+  g[0] = Q[2];
+  g[1] = irho * Q[2] * Q[1];
+  g[2] = irho * Q[2] * Q[2] + p;
+  g[3] = irho * Q[2] * Q[3];
+  g[4] = irho * Q[2] * (Q[4] + p);
+
+}
+
+
+void Euler::ADERDG::source(const double* const Q, double* S) {
+  // Number of variables = 5 + 0
+  // @todo Please implement
+  S[0] = 0.0;
+  S[1] = 0.0;
+  S[2] = 0.0;
+  S[3] = 0.0;
+  S[4] = 0.0;
+}
+
+
+void Euler::ADERDG::eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda) {
+  // Dimensions             = 2
+  // Number of variables    = 5 (#unknowns + #parameters)
+  const double GAMMA = 1.4;
+
+  double irho = 1.0 / Q[0];
+  double p = (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2]) * irho);
+
+  double u_n = Q[normalNonZeroIndex + 1] * irho;
+  double c = std::sqrt(GAMMA * p * irho);
+
+  lambda[0] = u_n - c;
+  lambda[1] = u_n;
+  lambda[2] = u_n;
+  lambda[3] = u_n;
+  lambda[4] = u_n + c;
+}
+
+
+void Euler::ADERDG::boundaryValues(const double* const x,const double t, const int faceIndex, const int normalNonZero, const double * const fluxIn, const double* const stateIn, double *fluxOut, double* stateOut) {
+  fluxOut[0] = fluxIn[0];
+  fluxOut[1] = fluxIn[1];
+  fluxOut[2] = fluxIn[2];
+  fluxOut[3] = fluxIn[3];
+  fluxOut[4] = fluxIn[4];
+
+  stateOut[0] = stateIn[0];
+  stateOut[1] = stateIn[1];
+  stateOut[2] = stateIn[2];
+  stateOut[3] = stateIn[3];
+  stateOut[4] = stateIn[4];
+}
+
+
+bool Euler::ADERDG::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double> &center, const tarch::la::Vector<DIMENSIONS, double> &dx, double t) {
+  if (tarch::la::equals(t, 0.0)) {
+    return true;
+  }
+  return false;
+}
+
+
+exahype::solvers::Solver::RefinementControl Euler::ADERDG::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t, const int level) {
+  // @todo Please implement
+  return exahype::solvers::Solver::RefinementControl::Keep;
+}
+
+
+void Euler::ADERDG::adjustedSolutionValues(const double* const x,const double w,const double t,const double dt,double* Q) {
+  // Dimensions             = 2
+  // Number of variables    = 5 (#unknowns + #parameters)
+  // @todo Please implement
+/*   
+  Q[0] = 0.0;
+  Q[1] = 0.0;
+  Q[2] = 0.0;
+  Q[3] = 0.0;
+  if((x[0] -0.5) *(x[0] -0.5) + (x[1] -0.5) *(x[1] -0.5) < 0.1) {
+    Q[4] = 1.0;
+  } else {
+    Q[4] = 0.1;
+  }
+   */
+    const double GAMMA = 1.4;
+    Q[0] = 1.;
+    Q[1] = 0.;
+    Q[2] = 0.;
+    Q[3] = 0.;
+    Q[4] =
+        1. / (GAMMA -1) +
+        std::exp(-((x[0] -0.5) *(x[0] -0.5) + (x[1] -0.5) *(x[1] -0.5) ) /
+        (0.05 *0.05)) *
+        1.0e-1;
+}
+
+
+
+
---
+++ Code/Applications/EulerFlow_ADERDGvsFVM/FVM.cpp
@@ -0,0 +1,101 @@
+#include "FVM.h"
+
+
+
+void Euler::FVM::init(){}
+
+void Euler::FVM::flux(const double* const Q, double** F) {
+  
+  const double GAMMA = 1.4;
+
+  const double irho = 1.0 / Q[0];
+  const double p =
+      (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2]) * irho);
+
+  double* f = F[0];
+  double* g = F[1];
+
+  // @todo Please implement
+  // f
+  f[0] = Q[1];
+  f[1] = irho * Q[1] * Q[1] + p;
+  f[2] = irho * Q[1] * Q[2];
+  f[3] = irho * Q[1] * Q[3];
+  f[4] = irho * Q[1] * (Q[4] + p);
+  // g
+  // @todo Please implement
+  g[0] = Q[2];
+  g[1] = irho * Q[2] * Q[1];
+  g[2] = irho * Q[2] * Q[2] + p;
+  g[3] = irho * Q[2] * Q[3];
+  g[4] = irho * Q[2] * (Q[4] + p);
+}
+
+
+void Euler::FVM::source(const double* const Q, double* S) {
+  S[0] = 0.0;
+  S[1] = 0.0;
+  S[2] = 0.0;
+  S[3] = 0.0;
+  S[4] = 0.0;
+}
+
+
+void Euler::FVM::eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda) {
+  
+  const double GAMMA = 1.4;
+
+  double irho = 1.0 / Q[0];
+  double p = (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2]) * irho);
+
+  double u_n = Q[normalNonZeroIndex + 1] * irho;
+  double c = std::sqrt(GAMMA * p * irho);
+
+  lambda[0] = u_n - c;
+  lambda[1] = u_n;
+  lambda[2] = u_n;
+  lambda[3] = u_n;
+  lambda[4] = u_n + c;
+}
+
+
+bool Euler::FVM::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t) {
+  if ( tarch::la::equals(t,0.0) ) {
+    return true;
+  }
+  return false; 
+}
+
+
+exahype::solvers::Solver::RefinementControl Euler::FVM::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center,const tarch::la::Vector<DIMENSIONS, double>& dx, double t,const int level) {
+  // @todo Please implement
+  return exahype::solvers::Solver::RefinementControl::Keep;
+}
+
+
+void Euler::FVM::adjustedSolutionValues(const double* const x, const double w, const double t, const double dt, double* Q) {  
+/*
+  Q[0] = 1.0;
+  Q[1] = 0.0;
+  Q[2] = 0.0;
+  Q[3] = 0.0;
+  if((x[0] -0.5) *(x[0] -0.5) + (x[1] -0.5) *(x[1] -0.5) < 0.1) {
+    Q[4] = 1.0;
+  } else {
+    Q[4] = 0.1;
+  }
+  //Q[4] = floor(10/(1+10*sqrt((x[0] -0.5) *(x[0] -0.5) + (x[1] -0.5) *(x[1] -0.5))));
+*/
+    const double GAMMA = 1.4;
+    Q[0] = 1.;
+    Q[1] = 0.;
+    Q[2] = 0.;
+    Q[3] = 0.;
+    Q[4] =
+        1. / (GAMMA -1) +
+        std::exp(-((x[0] -0.5) *(x[0] -0.5) + (x[1] -0.5) *(x[1] -0.5) ) /
+        (0.05 *0.05)) *
+        1.0e-1;
+
+}
+
