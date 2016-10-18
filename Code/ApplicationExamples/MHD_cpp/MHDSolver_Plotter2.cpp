e8e0a972b162fe4aa28a68f25f694f1735de0a3c
domcha <dominic.charrier@googlemail.com>
2016-10-18 14:58:48 +0100

    MHD_cpp: Add plotters.

---
+++ Code/ApplicationExamples/MHD_cpp/MHDSolver_Plotter2.cpp
@@ -0,0 +1,39 @@
+#include "MHDSolver_Plotter2.h"
+
+#include "MHDSolver.h"
+
+MHDSolver::MHDSolver_Plotter2::MHDSolver_Plotter2(MHDSolver&  solver) {
+  // @todo Please insert your code here
+}
+
+
+MHDSolver::MHDSolver_Plotter2::~MHDSolver_Plotter2() {
+  // @todo Please insert your code here
+}
+
+
+void MHDSolver::MHDSolver_Plotter2::startPlotting(double time) {
+  // @todo Please insert your code here
+}
+
+
+void MHDSolver::MHDSolver_Plotter2::finishPlotting() {
+  // @todo Please insert your code here
+}
+
+
+void MHDSolver::MHDSolver_Plotter2::mapQuantities(
+    const tarch::la::Vector<DIMENSIONS, double>& offsetOfPatch,
+    const tarch::la::Vector<DIMENSIONS, double>& sizeOfPatch,
+    const tarch::la::Vector<DIMENSIONS, double>& x,
+    const tarch::la::Vector<DIMENSIONS, int>&    pos,
+    double* Q,
+    double* outputQuantities,
+    double timeStamp
+) {
+  // MHD primitive variable plotter
+  int i;
+  MHDSolver::Cons2Prim(Q, outputQuantities);
+}
+
+
