084cf76c52e8f0d69128d7b0c528bfd1491a84be
Sven K <sven@nils.local>
2016-11-21 17:50:57 +0100

    MHD_TwoKernels: It now compiles checking Riemann Solver from both Fortran and C++.

---
+++ Code/ApplicationExamples/MHD_TwoKernels/PDE_shiv.f90
@@ -0,0 +1,18 @@
+! Just for because it is used at some strange place.
+
+SUBROUTINE PDENCP(BgradQ,Q,gradQ) 
+  ! not used  
+  PRINT *, 'PDENCP is not (yet) used for nonlinear-solvers' 
+  CALL EXIT  
+  !
+END SUBROUTINE PDENCP 
+ 
+
+
+SUBROUTINE PDEMatrixB(Bn,Q,nv) 
+  ! not used  
+  PRINT *, 'PDEMatrixB is not (yet) used for nonlinear-solvers' 
+  CALL EXIT  
+  !
+END SUBROUTINE PDEMatrixB 
+
---
+++ Code/ApplicationExamples/MHD_TwoKernels/README.txt
@@ -0,0 +1,7 @@
+Make sure you set
+
+export MIXEDLANG=Yes
+
+or put it into the Makefile. Otherwise Fortran kernels will not be load
+to ffiles.mk.
+
