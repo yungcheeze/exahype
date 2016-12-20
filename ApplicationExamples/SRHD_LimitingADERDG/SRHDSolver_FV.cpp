c1b3ff2f7c5f7ba5a8ba9b1167ffe3af9cecfdb9
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2016-12-20 11:50:34 +0100

    SRHD_LimitingADERDG: Add SRHD project using limiting ADER-DG solver.

---
+++ ApplicationExamples/SRHD_LimitingADERDG/SRHDSolver_FV.cpp
@@ -0,0 +1,88 @@
+#include "SRHDSolver_FV.h"
+
+#include <memory>
+
+using std::endl;
+using std::cout;
+
+extern "C" {
+void hastoadjustsolution_(double* time, bool* refine);
+void adjustedsolutionvalues_(const double* const x,const double* w,const double* t,const double* dt,double* Q);
+void pdeflux_(double* F, const double* const Q);
+void pdeeigenvalues_(double* lambda, const double* const Q, const double* nv);
+}
+
+
+void SRHD::SRHDSolver_FV::adjustedSolutionValues(const double* const x,const double w,const double t,const double dt,double* Q) {
+  if ( tarch::la::equals(t,0.0) ) {
+  // @todo Please implement and set initial conditions
+    adjustedsolutionvalues_(x, &w, &t, &dt, Q);
+  }
+  // @todo Feel free to add further conditions
+}
+
+
+bool SRHD::SRHDSolver_FV::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, double t, double dt) {
+  // @todo Please implement
+  if ( tarch::la::equals(t,0.0) ) {
+    // Tell kernel that you want to set initial conditions 
+    return true;
+  }
+  else {
+    // @todo Please implement
+    return false; 
+  }
+}
+
+
+exahype::solvers::Solver::RefinementControl SRHD::SRHDSolver_FV::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center,const tarch::la::Vector<DIMENSIONS, double>& dx, double t,const int level) {
+  // @todo Please implement
+  return exahype::solvers::Solver::RefinementControl::Keep;
+}
+
+
+void SRHD::SRHDSolver_FV::eigenvalues(const double* const Q, const int normalNonZeroIndex, double* lambda) {
+  // @todo Please implement
+  double nv[3] = {0.};
+  nv[normalNonZeroIndex] = 1;
+  pdeeigenvalues_(lambda, Q, nv);
+}
+
+
+void SRHD::SRHDSolver_FV::flux(const double* const Q, double** F) {
+  // @todo Please implement
+  pdeflux_(F[0], Q);
+}
+
+
+void SRHD::SRHDSolver_FV::source(const double* const Q, double* S) {
+  S[0] = 0.;
+  S[1] = 0.;
+  S[2] = 0.;
+  S[3] = 0.;
+  S[4] = 0.;
+}
+
+void SRHD::SRHDSolver_FV::boundaryValues(
+    const double* const x,
+    const double t,const double dt,
+    const int faceIndex,
+    const int normalNonZero,
+    const double* const stateIn,
+    double* stateOut) {
+
+  // The problem with these definitions is that in a simulation
+  // with a global nonzero velocity (as in MovingGauss2D), errnous
+  // values move into the simulation domain very quickly. So these
+  // boundary conditions are not good at all. Instead, we should
+  // have per default "vacuum" boundary conditions, so that vacuum
+  // values enter the grid as soon as matter moves away.
+
+  //  // stateOut
+  //  // @todo Please implement
+  stateOut[0] = stateIn[0];
+  stateOut[1] = stateIn[1];
+  stateOut[2] = stateIn[2];
+  stateOut[3] = stateIn[3];
+  stateOut[4] = stateIn[4];
+}
