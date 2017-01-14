ba9ffb6961225dcdb8e7e10f56b96629d9850dcc
Sven Köppel <koeppel@fias.uni-frankfurt.de>
2017-01-14 23:54:54 +0100

    Exa build tool installation script: added option to install several exahype repository installations at the same time.

--- Miscellaneous/BuildScripts/install-exa.sh
+++ Miscellaneous/BuildScripts/install-exa.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# optional command line argument: how to call the binary
+exaname="${1:-exa}"
+
 cd "$(dirname "$0")"
 
-if exaloc=$(which exa 2>&1); then
+
+if exaloc=$(which $exaname 2>&1); then
 	echo "You already have 'exa' installed at $exaloc"
 	ls -l $exaloc
-	echo "Don't do anything."
-	# todo: Could install another exa with an appendix
+	echo "Use '$0 [name of new exa]' to install exa with an alias (example: 'exa-disk2')"
 else
 	mkdir -p ~/bin
 	buildscripts=$PWD
 	cd ~/bin
 	ln -s $buildscripts/exa.sh
-	mv exa.sh exa
-	echo "Installed exa to ~/bin:"
-	ls -l exa
+	mv exa.sh $exaname
+	echo "Installed $exaname to ~/bin:"
+	ls -l $exaname
 	echo "Just use it by typing:"
-	which exa || { echo "Please log out and log in again to make it work."; }
+	which $exaname || { echo "Please log out and log in again to make it work."; }
 fi
