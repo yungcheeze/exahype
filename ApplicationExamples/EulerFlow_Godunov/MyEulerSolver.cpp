bac4d63d68d6273a07f352035c0a8d279cb4dbeb
domcha <dominic.charrier@googlemail.com>
2017-01-30 20:27:31 +0000

    EulerFlow_Godunov: Add variables wrapper. Code slows down from 29 sec to 32 sec. Not coupled to toolkit yet.

--- ApplicationExamples/EulerFlow_Godunov/MyEulerSolver.cpp
+++ ApplicationExamples/EulerFlow_Godunov/MyEulerSolver.cpp
@@ -60,45 +60,59 @@ void EulerFVM::MyEulerSolver::eigenvalues(const double* const Q, const int norma
 void EulerFVM::MyEulerSolver::flux(const double* const Q, double** F) {
   // Dimensions             = 2/3
   // Number of variables    = 5 (#unknowns + #parameters)
+  State state(Q);
+  Flux flux(F);
 
   const double GAMMA = 1.4;
+  const double irho = 1.0 / state.rho();
 
-  const double irho = 1.0 / Q[0];
-  #ifdef Dim2
-  double p = (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2]) * irho);
-  #else
-  double p = (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2] + Q[3] * Q[3]) * irho);
-  #endif
+  double p = (GAMMA-1) * ( state.E() - 0.5 * irho*state.u()*state.u() );
 
-  double* f = F[0];
-  double* g = F[1];
+  flux.writeRow(0, state.u());
+  tarch::la::Vector<3,double> pContribution(0.0,0.0,0.0);
+  for (int i=0; i<3; i++) {
+    pContribution[i] = p;
+    flux.writeRow(i+1, irho*state.u()[i]*state.u() + pContribution);
+    pContribution[i] = 0.0;
+  }
+  flux.writeRow(1+3, irho*state.u()*(state.E()+p));
 
-  // @todo Please implement
-  // f
-  f[0] = Q[1];
-  f[1] = irho * Q[1] * Q[1] + p;
-  f[2] = irho * Q[1] * Q[2];
-  f[3] = irho * Q[1] * Q[3];
-  f[4] = irho * Q[1] * (Q[4] + p);
-  // g
-  // @todo Please implement
-  g[0] = Q[2];
-  g[1] = irho * Q[2] * Q[1];
-  g[2] = irho * Q[2] * Q[2] + p;
-  g[3] = irho * Q[2] * Q[3];
-  g[4] = irho * Q[2] * (Q[4] + p);
-
-#ifdef Dim3
-  double* h = F[2];
-  // h
-  // @todo Please implement
-  h[0] = Q[3];
-  h[1] = irho * Q[3] * Q[1];
-  h[2] = irho * Q[3] * Q[2];
-  h[3] = irho * Q[3] * Q[3] + p;
-  h[4] = irho * Q[3] * (Q[4] + p);
-#endif
-
+//  const double GAMMA = 1.4;
+//  const double irho = 1.0 / Q[0];
+//  #ifdef Dim2
+//  double p = (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2]) * irho);
+//  #else
+//  double p = (GAMMA - 1) * (Q[4] - 0.5 * (Q[1] * Q[1] + Q[2] * Q[2] + Q[3] * Q[3]) * irho);
+//  #endif
+//
+//  double* f = F[0];
+//  double* g = F[1];
+//
+////   @todo Please implement
+////   f
+//  f[0] = Q[1];
+//  f[1] = irho * Q[1] * Q[1] + p;
+//  f[2] = irho * Q[1] * Q[2];
+//  f[3] = irho * Q[1] * Q[3];
+//  f[4] = irho * Q[1] * (Q[4] + p);
+////   g
+////   @todo Please implement
+//  g[0] = Q[2];
+//  g[1] = irho * Q[2] * Q[1];
+//  g[2] = irho * Q[2] * Q[2] + p;
+//  g[3] = irho * Q[2] * Q[3];
+//  g[4] = irho * Q[2] * (Q[4] + p);
+//
+//#ifdef Dim3
+//  double* h = F[2];
+////   h
+////   @todo Please implement
+//  h[0] = Q[3];
+//  h[1] = irho * Q[3] * Q[1];
+//  h[2] = irho * Q[3] * Q[2];
+//  h[3] = irho * Q[3] * Q[3] + p;
+//  h[4] = irho * Q[3] * (Q[4] + p);
+//#endif
 }
 
 
@@ -146,4 +160,6 @@ void EulerFVM::MyEulerSolver::boundaryValues(
   stateOut[3] = stateIn[3];
   stateOut[4] = stateIn[4];
 }
+
+
 
