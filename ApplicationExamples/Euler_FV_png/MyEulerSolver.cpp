a5049eb01bbcc125c437c981da70058b4c944b96
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-02-08 23:41:43 +0000

    Restored Tobias' Demonstrator version. Managed to get Logo.h down to 320 KB. It just reqired to change the original image to indexing mode (2 colours), the development and application of a  novel preprocessor based compression techniqe and lastly the inclusion of 205 third party libraries.

--- ApplicationExamples/Euler_FV_png/MyEulerSolver.cpp
+++ ApplicationExamples/Euler_FV_png/MyEulerSolver.cpp
@@ -4,10 +4,23 @@
  */
 #include "MyEulerSolver.h"
 #include "MyEulerSolver_Variables.h"
-#include "Logo.h"
+#include "picopng.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+picopng::Image Image;
 
 void EulerFV::MyEulerSolver::init(std::vector<std::string>& cmdlineargs) {
+	// Reading in Initial Data at startup
+	// You might pass your own PNG file on the command line next to the specfile.
+	const char* imagename = cmdlineargs.size() > 1 ? cmdlineargs.back().c_str() : "logo.png";
+	int ret = Image.loadPNG(imagename);
+	if(ret) {
+		fprintf(stderr, "Please ensure image '%s' is readable and a proper PNG file (err %d)\n", imagename, ret);
+		exit(-1);
+	}
 }
 
 bool EulerFV::MyEulerSolver::hasToAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, const double t, const double dt) {
@@ -21,18 +34,27 @@ void EulerFV::MyEulerSolver::adjustedSolutionValues(const double* const x,
                                                   const double dt, double* Q) {
   Variables vars(Q);
   
-  tarch::la::Vector<DIMENSIONS,double> myX( x[0] - 0.06, 1.0-x[1] - 0.25 ); // translate
+  tarch::la::Vector<DIMENSIONS,double> myX( x[0], 1.0-x[1] );
   myX *= static_cast<double>(Image.width);
-  tarch::la::Vector<DIMENSIONS,int>    myIntX( 1.2*myX(0) , 1.2*myX(1) );  // scale
+  tarch::la::Vector<DIMENSIONS,int>    myIntX( myX(0), myX(1) );
 
   double Energy = 0.1;
 
   if (
-    myIntX(0) > 0 && myIntX(0) < static_cast<int>(Image.width)
+    myIntX(0) < static_cast<int>(Image.width)
     &&
-    myIntX(1) > 0 && myIntX(1) < static_cast<int>(Image.height)
+    myIntX(1) < static_cast<int>(Image.height)
   ) {
-    Energy += 1.0-Image.pixel_data[myIntX(1)*Image.width+myIntX(0)];
+    Energy += (
+        Image.pixel_data[myIntX(1)*Image.width*4+myIntX(0)*4+0]
+      + Image.pixel_data[myIntX(1)*Image.width*4+myIntX(0)*4+1]
+      + Image.pixel_data[myIntX(1)*Image.width*4+myIntX(0)*4+2]) / 3.0 / 256.0;
+  }
+  else {
+    Energy += (
+        Image.pixel_data[0]
+      + Image.pixel_data[1]
+      + Image.pixel_data[2]) / 3.0 / 256.0;
   }
 
   vars.rho() = 1.0;
