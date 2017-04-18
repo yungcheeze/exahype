094bb7266f85227fe16ee60919e10314260f16c3
Sven K <sven@nils.local>
2017-04-18 13:59:34 +0200

    Exa build tools: Installation routine improvement, new toolkit mini compilation command.

--- Miscellaneous/BuildScripts/install-exa.sh
+++ Miscellaneous/BuildScripts/install-exa.sh
@@ -3,6 +3,10 @@
 # optional command line argument: how to call the binary
 exaname="${1:-exa}"
 
+# in order to install at another location, use like
+#   target="/usr/local/bin" .../install-exa.sh"
+target="${target:=$HOME/bin}"
+
 cd "$(dirname "$0")"
 
 
@@ -11,12 +15,12 @@ if exaloc=$(which $exaname 2>&1); then
 	ls -l $exaloc
 	echo "Use '$0 [name of new exa]' to install exa with an alias (example: 'exa-disk2')"
 else
-	mkdir -p ~/bin
+	mkdir -p $target
 	buildscripts=$PWD
-	cd ~/bin
+	cd $target
 	ln -s $buildscripts/exa.sh
 	mv exa.sh $exaname
-	echo "Installed $exaname to ~/bin:"
+	echo "Installed $exaname to $target:"
 	ls -l $exaname
 	echo "Just use it by typing:"
 	which $exaname || { echo "Please log out and log in again to make it work."; }
