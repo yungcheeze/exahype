1268e62df7b3759bdb2625ed64d04f9cca3d0f03
Kenneth Duru <kduru@eiger.geophysik.uni-muenchen.de>
2017-04-24 19:45:39 +0200

    extended physically motivated numerical flux to 2D linear elastodynamics. Boundary conditions are not accounted for yet.

---
+++ ApplicationExamples/Elastodynamics2D/MyElastodynamicsSolver.cpp
@@ -0,0 +1,437 @@
+#include "MyElastodynamicsSolver.h"
+
+#include "kernels/KernelUtils.h"
+
+#include "kernels/aderdg/generic/Kernels.h"
+
+
+#include "MyElastodynamicsSolver_Variables.h"
+
+#include <algorithm>
+
+tarch::logging::Log Elastodynamics::MyElastodynamicsSolver::_log( "Elastodynamics::MyElastodynamicsSolver" );
+
+
+void Elastodynamics::MyElastodynamicsSolver::init(std::vector<std::string>& cmdlineargs) {
+  // @todo Please implement/augment if required
+}
+
+exahype::solvers::ADERDGSolver::AdjustSolutionValue Elastodynamics::MyElastodynamicsSolver::useAdjustSolution(const tarch::la::Vector<DIMENSIONS,double>& center,const tarch::la::Vector<DIMENSIONS,double>& dx,const double t,const double dt) const {
+  // @todo Please implement/augment if required
+  return tarch::la::equals(t,0.0) ? exahype::solvers::ADERDGSolver::AdjustSolutionValue::PointWisely : exahype::solvers::ADERDGSolver::AdjustSolutionValue::No;
+}
+
+void Elastodynamics::MyElastodynamicsSolver::adjustPointSolution(const double* const x,const double w,const double t,const double dt,double* Q) {
+  // Dimensions             = 2
+  // Number of variables    = 5 + #parameters
+  
+  // @todo Please implement/augment if required
+  // State variables:
+
+  static tarch::logging::Log _log("MyElastodynamicsSolver::adjustPointSolution");
+   Variables vars(Q);
+   
+  Q[0] = std::exp(-((x[0]-0.5)*(x[0]-0.5)+(x[1]-0.5)*(x[1]-0.5))/0.01);
+  Q[1] = std::exp(-((x[0]-0.5)*(x[0]-0.5)+(x[1]-0.5)*(x[1]-0.5))/0.01);
+  Q[2] = 0.0;
+  Q[3] = 0.0;
+  Q[4] = 0.0;
+}
+
+void Elastodynamics::MyElastodynamicsSolver::eigenvalues(const double* const Q,const int d,double* lambda) {
+  // Dimensions             = 2
+  // Number of variables    = 5 + #parameters
+  
+  // @todo Please implement/augment if required
+  double cp = 6.0;
+  double cs = 3.343;
+  
+  lambda[0] = cp;
+  lambda[1] = cs;
+  lambda[2] = -cp;
+  lambda[3] = -cs;
+  lambda[4] = 0.0;
+}
+
+
+void Elastodynamics::MyElastodynamicsSolver::flux(const double* const Q,double** F) {
+  // Dimensions             = 2
+  // Number of variables    = 5 + #parameters
+  
+  // @todo Please implement/augment if required
+  F[0][0] = 0.0;
+  F[0][1] = 0.0;
+  F[0][2] = 0.0;
+  F[0][3] = 0.0;
+  F[0][4] = 0.0;
+
+  F[1][0] = 0.0;
+  F[1][1] = 0.0;
+  F[1][2] = 0.0;
+  F[1][3] = 0.0;
+  F[1][4] = 0.0;
+}
+
+
+void Elastodynamics::MyElastodynamicsSolver::boundaryValues(const double* const x,const double t,const double dt,const int faceIndex,const int normalNonZero,
+  const double * const fluxIn,const double* const stateIn,
+  double *fluxOut,double* stateOut) {
+  // Dimensions             = 2
+  // Number of variables    = 5 + #parameters
+
+  // @todo Please implement/augment if required
+  stateOut[0] = stateIn[0];
+  stateOut[1] = stateIn[1];
+  stateOut[2] = 0*stateIn[2];
+  stateOut[3] = 0*stateIn[3];
+  stateOut[4] = 0*stateIn[4];
+
+  fluxOut[0] = 0*fluxIn[0];
+  fluxOut[1] = 0*fluxIn[1];
+  fluxOut[2] = fluxIn[2];
+  fluxOut[3] = fluxIn[3];
+  fluxOut[4] = fluxIn[4];
+}
+
+
+exahype::solvers::Solver::RefinementControl Elastodynamics::MyElastodynamicsSolver::refinementCriterion(const double* luh,const tarch::la::Vector<DIMENSIONS,double>& center,const tarch::la::Vector<DIMENSIONS,double>& dx,double t,const int level) {
+  // @todo Please implement/augment if required
+  return exahype::solvers::Solver::RefinementControl::Keep;
+}
+
+
+void Elastodynamics::MyElastodynamicsSolver::nonConservativeProduct(const double* const Q,const double* const gradQ,double* BgradQ){
+  kernels::idx2 idx(DIMENSIONS, NumberOfVariables);
+
+  static tarch::logging::Log _log("Elastodynamics::MyElastodynamicsSolver::nonConservativeProduct");
+  ReadOnlyVariables vars(Q);
+
+  const double* Qx = &gradQ[0]; // (:,1)
+  const double* Qy = &gradQ[5]; // (:,2)
+  
+  double cp = 6.0;
+  double cs = 3.343;
+  
+  double rho = 2.7;
+  
+  double mu = rho*cs*cs;
+  double lam = rho*cp*cp-2.0*mu;
+  
+  BgradQ[0] = -1/rho*Qx[2];
+  BgradQ[1] = -1/rho*Qx[4];
+  BgradQ[2] = -(lam + 2.0*mu)*Qx[0];
+  BgradQ[3] = -0.0;
+  BgradQ[4] = -mu*Qx[1];
+
+  BgradQ[5] = -1/rho*Qy[4];
+  BgradQ[6] = -1/rho*Qy[3];
+  BgradQ[7] = -0.0;
+  BgradQ[8] = -(lam + 2*mu)*Qy[1];
+  BgradQ[9] = -mu*Qy[0];
+
+}
+
+
+
+void Elastodynamics::MyElastodynamicsSolver::coefficientMatrix(const double* const Q,const int d,double* Bn){
+  static tarch::logging::Log _log("MyElastodynamicsSolver::coefficientMatrix");
+
+  double cp = 6.0;
+  double cs = 3.343;
+  
+  double rho = 2.7;
+  
+  double mu = rho*cs*cs;
+  double lam = rho*cp*cp - 2*mu;
+  
+  double nv[2] = {0.0, 0.0};
+
+  nv[d] = 1.0;
+  
+  double B1[5][5];
+  double B2[5][5];
+   
+  for(int i=0; i<5; i++) {
+    for(int j=0; j<5; j++) {
+      B1[i][j] = 0.0;
+      B2[i][j] = 0.0;
+    }
+  }
+  
+  B1[0][2] = -1/rho; 
+  B1[1][4] = -1/rho;
+  B1[2][0] = -(2*mu + lam); 
+  B1[4][1] = -mu;
+
+  B2[0][4] = -1/rho; 
+  B2[1][3] = -1/rho;
+  B2[3][1] = -(2*mu + lam); 
+  B2[4][0] = -mu;
+  
+  for(int i=0; i<5; i++) {
+    for(int j=0; j<5; j++) {
+      
+      Bn[i*5 + j] = nv[0]*B1[i][j] + nv[1]*B2[i][j];
+    }
+  }
+
+}
+
+
+void Elastodynamics::MyElastodynamicsSolver::riemannSolver(double* FL,double* FR,const double* const QL,const double* const QR,double* tempFaceUnknownsArray,double** tempStateSizedVectors,double** tempStateSizedSquareMatrices,const double dt,const int normalNonZeroIndex){
+
+  constexpr int numberOfVariables  = MyElastodynamicsSolver::NumberOfVariables;
+  constexpr int numberOfVariables2 = numberOfVariables*numberOfVariables;
+  constexpr int numberOfParameters = MyElastodynamicsSolver::NumberOfParameters;
+  constexpr int numberOfData       = numberOfVariables+numberOfParameters;
+  constexpr int basisSize          = MyElastodynamicsSolver::Order+1;
+  constexpr int order              = basisSize - 1;
+  
+  kernels::idx2 idx_QLR(basisSize, numberOfVariables);
+
+  kernels::idx2 idx_FLR(basisSize, numberOfVariables);
+
+  double n[2]={0,0};
+  n[normalNonZeroIndex]=1;
+
+  double cp = 6.0;
+  double cs = 3.343;
+  
+  double rho = 2.7;
+  
+  double mu = rho*cs*cs;
+  double lam = rho*cp*cp - 2*mu;
+  
+  for (int i = 0; i < basisSize; i++) {
+
+
+    // extract tractions and particle velocities
+    double sigma_m_xx =  QL[idx_QLR(i,2)];
+    double sigma_m_yy =  QL[idx_QLR(i,3)];
+    double sigma_m_xy =  QL[idx_QLR(i,4)];
+
+    double sigma_p_xx =  QR[idx_QLR(i,2)];
+    double sigma_p_yy =  QR[idx_QLR(i,3)];
+    double sigma_p_xy =  QR[idx_QLR(i,4)];
+
+    double Tx_m = n[0]*sigma_m_xx + n[1]*sigma_m_xy;
+    double Ty_m = n[0]*sigma_m_xy + n[1]*sigma_m_yy;
+
+    double Tx_p = n[0]*sigma_p_xx + n[1]*sigma_p_xy;
+    double Ty_p = n[0]*sigma_p_xy + n[1]*sigma_p_yy;
+
+    double vx_m = QL[idx_QLR(i,0)];
+    double vy_m = QL[idx_QLR(i,1)];
+
+    double vx_p = QR[idx_QLR(i,0)];
+    double vy_p = QR[idx_QLR(i,1)];
+
+    
+    // generate local orthonormal basis: n, m
+    double m[3] = {0,0,0};
+    double l[3] = {0,0,0};
+    
+    localBasis(n, m, l, 2);
+      //m[0] = n[1];
+      //m[1] = -n[0];
+
+    // rotate tractions and particle velocities into orthogonal coordinates: n, m
+    double Tn_m= Tx_m*n[0] + Ty_m*n[1];
+    double Tm_m= Tx_m*m[0] + Ty_m*m[1];
+    
+    double Tn_p= Tx_p*n[0] + Ty_p*n[1];
+    double Tm_p= Tx_p*m[0] + Ty_p*m[1];
+    
+    double vn_m= vx_m*n[0] + vy_m*n[1];
+    double vm_m= vx_m*m[0] + vy_m*m[1];
+    
+    double vn_p= vx_p*n[0] + vy_p*n[1];
+    double vm_p= vx_p*m[0] + vy_p*m[1];
+
+   
+    // extract local s-wave and p-wave impedances
+    double zs_p=rho*cs;
+    double zs_m=rho*cs;
+
+    double zp_p=rho*cp;
+    double zp_m=rho*cp;
+
+    
+    // generate interface data preserving the amplitude of the outgoing charactertritics
+    // and satisfying interface conditions exactly.
+    double vn_hat_p=0;
+    double vm_hat_p=0;
+
+    double vn_hat_m=0;
+    double vm_hat_m=0;
+
+    double Tn_hat_p=0;
+    double Tm_hat_p=0;
+
+    double Tn_hat_m=0;
+    double Tm_hat_m=0;
+
+    // data is generated by solving a local Riemann problem and contraining the solutions against
+    // physical interface conditions
+    riemannSolver_Nodal(vn_p,vn_m, Tn_p, Tn_m, zp_p , zp_m, vn_hat_p , vn_hat_m, Tn_hat_p, Tn_hat_m);
+    riemannSolver_Nodal(vm_p,vm_m, Tm_p, Tm_m, zs_p , zs_m, vm_hat_p , vm_hat_m, Tm_hat_p, Tm_hat_m);
+
+    // generate fluctuations in the local basis coordinates: n, m
+    double FLn = 0.5*(zp_m*(vn_m-vn_hat_m) + (Tn_m-Tn_hat_m));
+    double FLm = 0.5*(zs_m*(vm_m-vm_hat_m) + (Tm_m-Tm_hat_m));
+
+    double FRn = 0.5*(zp_p*(vn_p-vn_hat_p) - (Tn_p-Tn_hat_p));
+    double FRm = 0.5*(zs_p*(vm_p-vm_hat_p) - (Tm_p-Tm_hat_p));
+
+    double FL_n = 0.5/zp_m*(zp_m*(vn_m-vn_hat_m) + (Tn_m-Tn_hat_m));
+    double FL_m = 0.5/zs_m*(zs_m*(vm_m-vm_hat_m) + (Tm_m-Tm_hat_m));
+
+    double FR_n = 0.5/zp_p*(zp_p*(vn_p-vn_hat_p) - (Tn_p-Tn_hat_p));
+    double FR_m = 0.5/zs_p*(zs_p*(vm_p-vm_hat_p) - (Tm_p-Tm_hat_p));
+
+    
+    // rotate back to the physical coordinates x, y
+    double FLx = n[0]*FLn + m[0]*FLm;
+    double FLy = n[1]*FLn + m[1]*FLm;
+
+    double FRx = n[0]*FRn + m[0]*FRm;
+    double FRy = n[1]*FRn + m[1]*FRm;
+
+    double FL_x = n[0]*FL_n + m[0]*FL_m;
+    double FL_y = n[1]*FL_n + m[1]*FL_m;
+
+    double FR_x = n[0]*FR_n + m[0]*FR_m;
+    double FR_y = n[1]*FR_n + m[1]*FR_m;
+     
+    // construct flux fluctuation vectors obeying the eigen structure of the PDE
+    // and choose physically motivated penalties such that we can prove
+    // numerical stability.
+
+    FR[idx_FLR(i, 0)] = 1/rho*FRx;
+    FL[idx_FLR(i, 0)] = 1/rho*FLx;
+    
+    FR[idx_FLR(i, 1)] = 1/rho*FRy;
+    FL[idx_FLR(i, 1)] = 1/rho*FLy;
+
+    FL[idx_FLR(i, 2)] = (2*mu+lam)*n[0]*FL_x + lam*n[1]*FL_y;
+    FL[idx_FLR(i, 3)] = (2*mu+lam)*n[1]*FL_y + lam*n[0]*FL_x;
+
+    FR[idx_FLR(i, 2)] = -((2*mu+lam)*n[0]*FR_x + lam*n[1]*FR_y);
+    FR[idx_FLR(i, 3)] = -((2*mu+lam)*n[1]*FR_y + lam*n[0]*FR_x);
+
+
+    FL[idx_FLR(i, 4)] =  mu*(n[1]*FL_x + n[0]*FL_y);
+    FR[idx_FLR(i, 4)] = -mu*(n[1]*FR_x + n[0]*FR_y);
+    
+  }
+  
+}
+
+
+void Elastodynamics::MyElastodynamicsSolver::pointSource(const double* const x,const double t,const double dt, double* forceVector, double* x0){
+     double pi = 3.14159265359;
+  double sigma = 0.1149;
+  double t0 = 0.7;
+  //double t0 = 0.1;
+  double f = 0.0;
+  double M0 = 1000.0;
+  
+
+  //f = M0*(1.0/(sigma*std::sqrt(2.0*pi)))*(std::exp(-pow((t-t0)/(std::sqrt(2)*sigma),2.0)));
+
+  f = M0*(1.0/(sigma*std::sqrt(2.0*pi)))*(std::exp(-((t-t0)*(t-t0))/(2.0*sigma*sigma)));
+
+
+  //f = M0*t/(t0*t0)*std::exp(-t/t0);
+
+  x0[0] = 0.5;
+  x0[1] = 0.5;
+
+  forceVector[0] = 1.*f;
+  forceVector[1] = 0.0;
+  forceVector[2] = 0.0;
+
+}
+
+
+void Elastodynamics::MyElastodynamicsSolver::riemannSolver_Nodal(double v_p,double v_m, double sigma_p, double sigma_m, double z_p , double z_m, double& v_hat_p , double& v_hat_m, double& sigma_hat_p, double& sigma_hat_m){
+   double p=0;
+   double q=0;
+   double phi=0;
+   double v_hat=0;
+   double eta=0;
+
+   p=z_m*v_p + sigma_p;
+   q=z_p*v_m - sigma_m;
+
+   eta=(z_p*z_m)/(z_p+z_m);
+
+   phi= eta*(p/z_p - q/z_m);
+
+   sigma_hat_p=phi;
+   sigma_hat_m=phi;
+
+   v_hat_m=(p-phi)/z_p;
+   v_hat_p=(q+phi)/z_m;
+
+ }
+
+
+void Elastodynamics::MyElastodynamicsSolver::Gram_Schmidt(double* y, double* z){
+  //Gram Schmidt orthonormalization
+ 
+  double  a_yz = y[0]*z[0] + y[1]*z[1] + y[2]*z[2];
+
+  for (int i = 0; i< 3; i++){
+    z[i] = z[i] - a_yz*y[i];
+    z[i] = 1.0/std::sqrt(z[0]*z[0] + z[1]*z[1] + z[2]*z[2])*z[i];
+  }
+
+}
+
+void Elastodynamics::MyElastodynamicsSolver::localBasis(double* n, double * m, double* l, int d){
+
+  if (d == 2)
+    {
+      l[0] = 0.;
+      l[1] = 0.;
+      l[2] = 1.0;
+      
+      m[0] = n[1]*l[2]-n[2]*l[1];
+      m[1] = -(n[0]*l[2]-n[2]*l[0]);
+      m[2] = n[0]*l[1]-n[1]*l[0];
+      
+    }
+  
+  
+  if (d == 3)
+    {
+      double tol, diff_norm1, diff_norm2;
+      
+      tol = 1e-12;
+      m[0] = 0.;
+      m[1] = 1.;
+      m[2] = 0.;
+      
+      diff_norm1 =  std::sqrt(pow(n[0]-m[0],2) + pow(n[1]-m[1], 2) + pow(n[2]-m[2], 2));
+      
+      diff_norm2 =  std::sqrt(pow(n[0]+m[0],2) + pow(n[1]+m[1], 2) + pow(n[2]+m[2], 2));
+      
+      if (diff_norm1 >= tol && diff_norm2 >= tol){
+	Gram_Schmidt(n, m);}	else
+	{
+	  m[0] = 0.;
+	  m[1] = 0.;
+	  m[2] = 1.;
+	  
+	  Gram_Schmidt(n, m);
+	  
+	}
+      
+      l[0] = n[1]*m[2]-n[2]*m[1];
+      l[1] = -(n[0]*m[2]-n[2]*m[0]);
+      l[2] = n[0]*m[1]-n[1]*m[0];
+      
+    }
+  
+}
