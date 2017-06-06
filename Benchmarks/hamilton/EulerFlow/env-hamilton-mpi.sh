7683a7fca0f8566874826a0bae9b4ff81191d3aa
domcha <dominic.charrier@googlemail.com>
2017-06-06 13:40:17 +0100

    Benchmarks: export TBB_SHLIB env variable.

--- Benchmarks/hamilton/EulerFlow/env-hamilton-mpi.sh
+++ Benchmarks/hamilton/EulerFlow/env-hamilton-mpi.sh
@@ -7,6 +7,8 @@ module load intel/xe_2017.2
 module load intelmpi/intel/2017.2
 module load gcc
 
+export TBB_SHLIB=/ddn/apps/Cluster-Apps/intel/xe_2017.2/tbb/lib/intel64/gcc4.7
+
 export I_MPI_FABRICS="shm:dapl"
 
 export EXAHYPE_CC=mpicc
--- Benchmarks/hamilton/EulerFlow/env-hamilton-nompi.sh
+++ Benchmarks/hamilton/EulerFlow/env-hamilton-nompi.sh
@@ -7,6 +7,8 @@ module load intel/xe_2017.2
 module load intelmpi/intel/2017.2
 module load gcc
 
+export TBB_SHLIB=/ddn/apps/Cluster-Apps/intel/xe_2017.2/tbb/lib/intel64/gcc4.7
+
 export EXAHYPE_CC=icpc
 
 export MODE=Release
