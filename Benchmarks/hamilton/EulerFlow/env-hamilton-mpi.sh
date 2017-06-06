c6d520df6f860fb44f741a77e0b3f43765e2bc33
domcha <dominic.charrier@googlemail.com>
2017-06-06 12:12:57 +0100

    Benchmarks: Updated hamilton env scripts.

--- Benchmarks/hamilton/EulerFlow/env-hamilton-mpi.sh
+++ Benchmarks/hamilton/EulerFlow/env-hamilton-mpi.sh
@@ -1,4 +1,11 @@
+source /etc/profile.d/modules.sh
+
+module purge
 module load java
+module load slurm
+module load intel/xe_2017.2
+module load intelmpi/intel/2017.2
+module load gcc
 
 export EXAHYPE_CC=mpicc
 
