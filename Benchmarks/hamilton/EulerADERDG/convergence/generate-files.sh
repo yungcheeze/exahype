3e61684add69f14dc5bfa4494f8edf0333ddd18d
domcha <dominic.charrier@googlemail.com>
2017-07-03 17:25:47 +0100

    More work on convergence scripts.

--- Benchmarks/hamilton/EulerADERDG/convergence/generate-files.sh
+++ Benchmarks/hamilton/EulerADERDG/convergence/generate-files.sh
@@ -22,8 +22,7 @@ project=EulerADERDG
 skipReductionInBatchedTimeSteps=on
 batchFactor=0.8
 hMax=(0.12 0.038 0.0124)
-T=(2.0 2.0 2.0)  # p=3
-io=no-output # or output
+T=(0.1 0.1 0.1)
 
 kernels=gen
 
@@ -31,7 +30,7 @@ kernels=gen
 
 for fuseAlgorithmicSteps in "on" "off"
 do
-prefix=$project-$io
+prefix=$project
 
 i=0
 mesh=regular-$i
@@ -52,21 +51,9 @@ fi
 
 for order in 3 5 7 9
 do
- # T=(0.01 0.002 0.0005 0.0001) # p=3
- # if (( order == 5 )); then
- #   T=(0.003)                    # p=5
- # fi
- # if (( order == 7 )); then
- #   T=(0.001)                    # p=9
- # fi
- # if (( order == 9 )); then
- #   T=(0.0003)                   # p=9
- # fi
-
-
   # Create script
   script=convergence/hamilton.slurm-script
-  newScript=convergence/hamilton-$prefix-p$order-n1-t1.slurm-script
+  newScript=convergence/hamilton-$prefix-p${order}.slurm-script
   cp $script $newScript
  
   sed -i 's,'$project'-no-output-regular-0,'$prefix',g' $newScript
@@ -84,7 +71,7 @@ do
   #for coresPerTask in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 32 # ham6
   do
     spec=convergence/EulerADERDG-$io.exahype
-    filename=convergence/$prefix-p$order-t1-c$coresPerTask
+    filename=convergence/$prefix-p$order
     newSpec=$filename'.exahype'
 
     cp $spec $newSpec
