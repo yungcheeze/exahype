8642e81409217d1399cff54dd246ded46f13ef1d
domcha <dominic.charrier@googlemail.com>
2017-07-03 18:58:25 +0100

    More refactoring.

--- Benchmarks/hamilton/EulerADERDG/convergence/clean.sh
+++ Benchmarks/hamilton/EulerADERDG/convergence/clean.sh
@@ -1,4 +1,4 @@
-directory=multicore
+directory=convergence
 
-rm ${directory}/hamilton*-*-n*-t*.slurm-script
-rm ${directory}/*-t*-c*.exahype
+rm ${directory}/hamilton-*.slurm-script
+rm ${directory}/EulerADERDG-*.exahype
--- Benchmarks/hamilton/EulerADERDG/convergence/generate-files.sh
+++ Benchmarks/hamilton/EulerADERDG/convergence/generate-files.sh
@@ -22,74 +22,70 @@ project=EulerADERDG
 skipReductionInBatchedTimeSteps=on
 batchFactor=0.8
 hMax=(0.12 0.038 0.0124)
-T=(0.1 0.1 0.1)
+T=(0.11 0.11 0.11)
 
 kernels=gen
 
 # Derived options
 
 for fuseAlgorithmicSteps in "on" "off"
-do
-prefix=$project
-
-i=0
-mesh=regular-$i
-h=${hMax[i]}
-t=${T[i]}
-
-prefix+=-$kernels-$mesh
-
-
-if [ "$fuseAlgorithmicSteps" == "on" ]; then
- prefix+="-fused"
-else
- prefix+="-nonfused"
-fi
-
-# prefix= project-io-kernels-mesh-i-
-# regex = .+-.+-(\w+)-.+-.+-
-
-for order in 3 5 7 9
-do
-  # Create script
-  script=convergence/hamilton.slurm-script
-  newScript=convergence/hamilton-$prefix-p${order}.slurm-script
-  cp $script $newScript
- 
-  sed -i 's,'$project'-no-output-regular-0,'$prefix',g' $newScript
-
-  sed -i 's,kernels=gen,kernels='$kernels',g' $newScript
-  
-  sed -i 's,p3,p'$order',g' $newScript
-  sed -i 's,regular-0,'$mesh',g' $newScript
-
-  sed -i 's,script=convergence/hamilton.slurm-script,script='$newScript',g' $newScript
-  
-  # Create spec files
-  for coresPerTask in 24
-  #for coresPerTask in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 48 # ham7
-  #for coresPerTask in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 32 # ham6
+do 
+  for i in 0 1  2
   do
-    spec=convergence/EulerADERDG-$io.exahype
-    filename=convergence/$prefix-p$order
-    newSpec=$filename'.exahype'
+    # prefix= project-kernels-mesh-i-
+    # regex = .+-.+-(\w+)-.+-.+-
+    for order in 3 5 7 9
+    do
+      prefix=$project
 
-    cp $spec $newSpec
-
-    if [[ "$kernels" == "opt" ]]; then
-      sed -i -r "s,generic::fluxes::nonlinear,optimised::fluxes::nonlinear," $newSpec
-    fi
-
-    sed -i -r 's,end-time(\s*)=(\s*)(([0-9]|\.)*),end-time\1=\2'$t',' $newSpec
-    sed -i -r 's,ranks_per_node:([0-9]+),ranks_per_node:1,' $newSpec 
-    sed -i -r 's,cores(\s+)=(\s+)([0-9]+),cores\1=\2'$coresPerTask',' $newSpec
+      mesh=regular-$i
+      h=${hMax[i]}
+      t=${T[i]}
+      
+      prefix+=-$kernels-$mesh
+      
+      if [ "$fuseAlgorithmicSteps" == "on" ]; then
+        prefix+="-fused"
+      else
+        prefix+="-nonfused"
+      fi
+      
+      # Create script
+      script=convergence/hamilton.slurm-script
+      newScript=convergence/hamilton-$prefix-p${order}.slurm-script
+      cp $script $newScript
    
-    sed -i -r 's,skip-reduction-in-batched-time-steps(\s*)=(\s*)(\w+),skip-reduction-in-batched-time-steps\1=\2'$skipReductionInBatchedTimeSteps',' $newSpec
-    sed -i -r 's,timestep-batch-factor(\s*)=(\s*)(([0-9]|\.)+),timestep-batch-factor\1=\2'$batchFactor',' $newSpec
-    sed -i -r 's,fuse-algorithmic-steps(\s*)=(\s*)(\w+),fuse-algorithmic-steps\1=\2'$fuseAlgorithmicSteps',' $newSpec
-  
-    sed -i -r 's,order(\s+)const(\s+)=(\s+)([0-9]+),order\1const\2=\3'$order',' $newSpec
-    sed -i -r 's,maximum-mesh-size(\s*)=(\s*)(([0-9]|\.)*),maximum-mesh-size\1=\2'$h',' $newSpec
+      sed -i 's,'$project'-no-output-regular-0,'$prefix',g' $newScript
+
+      sed -i 's,kernels=gen,kernels='$kernels',g' $newScript
+    
+      sed -i 's,p3,p'$order',g' $newScript
+      sed -i 's,regular-0,'$mesh',g' $newScript
+
+      sed -i 's,script=convergence/hamilton.slurm-script,script='$newScript',g' $newScript
+    
+      # Create spec files
+      coresPerTask=24
+      spec=convergence/EulerADERDG.exahype
+      filename=convergence/$prefix-p$order
+      newSpec=$filename'.exahype'
+
+      cp $spec $newSpec
+
+      if [[ "$kernels" == "opt" ]]; then
+        sed -i -r "s,generic::fluxes::nonlinear,optimised::fluxes::nonlinear," $newSpec
+      fi
+
+      sed -i -r 's,end-time(\s*)=(\s*)(([0-9]|\.)*),end-time\1=\2'$t',' $newSpec
+      sed -i -r 's,ranks_per_node:([0-9]+),ranks_per_node:1,' $newSpec 
+      sed -i -r 's,cores(\s+)=(\s+)([0-9]+),cores\1=\2'$coresPerTask',' $newSpec
+     
+      sed -i -r 's,skip-reduction-in-batched-time-steps(\s*)=(\s*)(\w+),skip-reduction-in-batched-time-steps\1=\2'$skipReductionInBatchedTimeSteps',' $newSpec
+      sed -i -r 's,timestep-batch-factor(\s*)=(\s*)(([0-9]|\.)+),timestep-batch-factor\1=\2'$batchFactor',' $newSpec
+      sed -i -r 's,fuse-algorithmic-steps(\s*)=(\s*)(\w+),fuse-algorithmic-steps\1=\2'$fuseAlgorithmicSteps',' $newSpec
+    
+      sed -i -r 's,order(\s+)const(\s+)=(\s+)([0-9]+),order\1const\2=\3'$order',' $newSpec
+      sed -i -r 's,maximum-mesh-size(\s*)=(\s*)(([0-9]|\.)*),maximum-mesh-size\1=\2'$h',' $newSpec
+    done
   done
-done
 done
