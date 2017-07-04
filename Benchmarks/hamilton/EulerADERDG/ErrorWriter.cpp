b9e504d445012c03d7af6bb5a6f09a887a248bb7
domcha <dominic.charrier@googlemail.com>
2017-07-04 11:50:03 +0100

    Fix bug in error writer.

--- Benchmarks/hamilton/EulerADERDG/ErrorWriter.cpp
+++ Benchmarks/hamilton/EulerADERDG/ErrorWriter.cpp
@@ -37,10 +37,10 @@ void EulerADERDG::ErrorWriter::plotPatch(
 
   kernels::idx4 idx(basisSize,basisSize,basisSize,numberOfVariables);
   dfor(i,basisSize) {
-     double w_dV = 0;
+     double w_dV = 1.0;
      for (int d=0; d<DIMENSIONS; d++) {
-       x[d] = offsetOfPatch[d] + sizeOfPatch[d] * kernels::gaussLegendreNodes[order][i(d)];
-       w_dV = sizeOfPatch[d] * kernels::gaussLegendreWeights[order][i(d)];
+       x[d]  = offsetOfPatch[d] + sizeOfPatch[d] * kernels::gaussLegendreNodes[order][i(d)];
+       w_dV *= sizeOfPatch[d] * kernels::gaussLegendreWeights[order][i(d)];
      }
 
      double uAna[numberOfVariables];
