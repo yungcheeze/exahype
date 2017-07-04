aee850122aa047575d5112d4e3768b236bcba577
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-07-05 00:49:26 +0100

    Fixed bug in sodshocktube script.

--- Miscellaneous/ConvergenceAnalysis/SodShockTube/postshockfluidpressure.py
+++ Miscellaneous/ConvergenceAnalysis/SodShockTube/postshockfluidpressure.py
@@ -1,81 +1,79 @@
 #!/usr/bin/python
 # Python(2) script for computing post shock fluid
 # pressure and velocity
-#reference: http://www.phys.lsu.edu/~tohline/PHYS7412/sod.html
+# reference 1: https://en.wikipedia.org/wiki/Sod_shock_tube
+# reference 2: https://gitlab.com/fantaz/Riemann_exact/tree/master
+# reference 3: Toro, E., Riemann Solvers and Numerical Methods for Fluid Dynamics (4.3.3 Numerical tests)
 from sympy import *
 from mpmath import mp
 
 mp.dps = 50
 
-P = symbols('P')
-
 # Initial data
-g=1.4
+gamma=1.4
 
-x0 = 0.5;
+x_0 = 0.5;
 
-rho_r = 0.125;
-P_r   = 0.1;
-u_r   = 0;
+rho_5 = 0.125; # right initial states
+P_5   = 0.1;
+u_5   = 0.0;
 
-rho_l = 1;
-P_l   = 1;
-u_l   = 0;
+rho_1 = 1.0;    # left initial states
+P_1   = 1.0;
+u_1   = 0.0;
 
-mu2=(g-1)/(g+1)
-mu = sqrt( mu2 );
+Gamma =(gamma-1)/(gamma+1)
+Gamma2=Gamma**2
 
-print "constexpr double gamma     =%1.20f;" % g
-print "constexpr double mu        =%1.20f;" % mu
-print "constexpr double x0        =%1.20f;\n" % x0
-print "constexpr double rho_r     =%1.20f;" % rho_r
-print "constexpr double P_r       =%1.20f;" % P_r
-print "constexpr double u_r       =%1.20f;" % u_r
-print "constexpr double rho_l     =%1.20f;" % rho_l
-print "constexpr double P_l       =%1.20f;" % P_l
-print "constexpr double u_l       =%1.20f;\n" % u_l
+beta = (gamma-1)/(2*gamma)
 
-c_l = sqrt( g*P_l/rho_l )
-c_r = sqrt( g*P_r/rho_r )
+print "constexpr double gamma     =%1.20f;"   % gamma
+print "constexpr double Gamma     =%1.20f;"   % Gamma
+print "constexpr double x_0       =%1.20f;\n" % x_0
+print "constexpr double rho_5     =%1.20f;"   % rho_5
+print "constexpr double P_5       =%1.20f;"   % P_5
+print "constexpr double u_5       =%1.20f;"   % u_5
+print "constexpr double rho_1     =%1.20f;"   % rho_1
+print "constexpr double P_1       =%1.20f;"   % P_1
+print "constexpr double u_1       =%1.20f;\n" % u_1
 
-print "constexpr double c_l       =%1.20f;" % c_l
-print "constexpr double c_r       =%1.20f;\n" % c_r
+cs_1 = sqrt( gamma*P_1/rho_1 )
+cs_5 = sqrt( gamma*P_5/rho_5 )
 
-expressionForP =  ( 
-  2*sqrt(g)/(g-1) * ( 1 - pow(P,(g-1)/(2*g)) ) 
+print "constexpr double cs_1       =%1.20f;"   % cs_1
+print "constexpr double cs_5       =%1.20f;\n" % cs_5
+
+P = symbols('P')
+expressionForP_3 =  ( 
+  (pow(P_1,beta)-pow(P,beta)) * sqrt( (1-Gamma2)*pow(P_1,gamma) / (Gamma2*rho_1) )
   -
-  ( P - P_r ) * sqrt( pow(1-mu2,2) / ( rho_r * (P + mu2*P_r ) ) )
+  (P-P_5) * sqrt( (1-Gamma) / ( rho_5*(P + Gamma*P_5) ) )
 )
 
-lambdifiedExpressionForP = lambdify(P, expressionForP, 'mpmath') 
-P_post = mpmath.findroot(lambdifiedExpressionForP, 0.3190)
+lambdifiedExpressionForP_3 = lambdify(P, expressionForP_3, 'mpmath') 
+P_3 = mpmath.findroot(lambdifiedExpressionForP_3, 0.3031)
+P_4 = P_3
 
-v_post = 2*sqrt(g)/(g - 1) * ( 1 - pow(P_post,(g-1)/(2*g)) )
+u_3   = u_5 + (P_3-P_5) / sqrt( rho_5/2 * ( (gamma+1)*P_3 + (gamma-1)*P_5 ) )
+u_4   = u_3
 
-rho_l = 1;
-P_l = 1;
-u_l = 0;
+u_2   =  (pow(P_1,beta)-pow(P_3,beta)) * sqrt( (1-Gamma2)*pow(P_1,gamma) / (Gamma2*rho_1) )
 
-rho_r = 0.125;
-P_r = 0.1;
-u_r = 0;
+rho_3 = rho_1 * pow(P_3/P_1,1.0/gamma)
+rho_4 = rho_5 * (P_4+Gamma*P_5)/(P_5+Gamma*P_4)
 
+print "constexpr double u_2       =%1.20f;\n"   % u_2
 
-print "constexpr double P_post    =%1.20f;" % P_post
-print "constexpr double v_post    =%1.20f;" % v_post
+cs_3 = sqrt( gamma*P_3/rho_3 )
 
-rho_l = 1;
-P_l = 1;
-u_l = 0;
+print "constexpr double rho_3     =%1.20f;"   % rho_3
+print "constexpr double P_3       =%1.20f;"   % P_3
+print "constexpr double u_3       =%1.20f;"   % u_3
+print "constexpr double cs_3      =%1.20f;\n" % cs_3
 
-rho_r = 0.125;
-P_r = 0.1;
-u_r = 0;
+print "constexpr double rho_4     =%1.20f;"   % rho_4
+print "constexpr double P_4       =%1.20f;"   % P_4
+print "constexpr double u_4       =%1.20f;\n"   % u_4
 
-rho_post   = rho_r*(( (P_post/P_r) + mu2 )/(1 + mu2*(P_post/P_r)));
-v_shock    = v_post*((rho_post/rho_r)/( (rho_post/rho_r) - 1.0));
-rho_middle = (rho_l)*pow((P_post/P_l),1.0/g);
-
-print "constexpr double rho_post  =%1.20f;" % rho_post
-print "constexpr double v_shock   =%1.20f;" % v_shock
-print "constexpr double rho_middle=%1.20f;" % rho_middle
+u_shock = cs_5 * sqrt( 1+(gamma+1)/(2*gamma)*(P_4/P_5-1) )
+print "constexpr double u_shock   =%1.20f;\n"   % u_shock
