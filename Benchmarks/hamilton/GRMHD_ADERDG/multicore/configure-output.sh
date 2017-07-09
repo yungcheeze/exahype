5d28c076392e125d9d0a655cfa603e9eeb0076a3
domcha <dominic.charrier@googlemail.com>
2017-07-09 21:04:43 +0100

    Output which libraries to link to.

--- Benchmarks/hamilton/GRMHD_ADERDG/multicore/configure-output.sh
+++ Benchmarks/hamilton/GRMHD_ADERDG/multicore/configure-output.sh
@@ -2,3 +2,7 @@ echo "Configure project for multicore scaling test (output)."
 ./projectpaths.cfg
 ( cd ../../../ && java -jar Toolkit/dist/ExaHyPE.jar --not-interactive Benchmarks/hamilton/GRMHD_ADERDG/multicore/GRMHD_ADERDG-output.exahype )
 mkdir multicore/results
+rm *.o
+
+printf "\n\nPlease add the following line to your Makefile:\n"
+echo "PROJECT_LFLAGS+=-lgsl -lgslcblas -lm"
