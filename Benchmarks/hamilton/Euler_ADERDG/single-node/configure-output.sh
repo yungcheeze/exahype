f531e4b96fa2422f7e0877c1cb530fa30664ea98
domcha <dominic.charrier@googlemail.com>
2017-07-13 14:39:34 +0100

    Work on reactivating single-node multi-rank Euler benchmarks.

--- Benchmarks/hamilton/Euler_ADERDG/single-node/configure-output.sh
+++ Benchmarks/hamilton/Euler_ADERDG/single-node/configure-output.sh
@@ -1,2 +1,4 @@
 echo "Configure project for single-node scaling test (output)."
 ( cd ../../../ && java -jar Toolkit/dist/ExaHyPE.jar --not-interactive Benchmarks/hamilton/Euler_ADERDG/single-node/Euler_ADERDG-output.exahype )
+mkdir single-node/results
+rm *.o
--- Benchmarks/hamilton/Euler_ADERDG/single-node/generate-files.sh
+++ Benchmarks/hamilton/Euler_ADERDG/single-node/generate-files.sh
@@ -18,65 +18,85 @@
 #   64 GB TruDDR4 memory
 #   the nodes are diskless
 #   1 x Intel OmniPath 100 Gb InfiniBand interconnect
-hMax=(0.05 0.01 0.005 0.001)
-times=(0.01 0.002 0.0005 0.0001)
 
-i=0
-mesh=regular-$i
-h=${hMax[i]}
-t=${times[i]}
-
+# PREAMBLE
 order=5
-
-sharedMem=TBB
-
 skipReductionInBatchedTimeSteps=on
 batchFactor=0.8
+io=no-output # or output
+kernels=gen
+sharedMem=TBB
 
-for io in 'output' 'no-output'
+# MESH
+i=0
+hMax=(0.05 0.01 0.005 0.001)
+mesh=regular-$i
+h=${hMax[i]}
+
+# SIMULATION END TIME
+T=(0.01 0.002 0.0005 0.0001)   # p=3
+if (( order == 5 )); then
+  T=(0.003)                    # p=5
+fi
+if (( order == 7 )); then
+  T=(0.001)                    # p=7
+fi
+if (( order == 9 )); then
+  T=(0.0003)                   # p=9
+fi
+t=${T[i]}
+
+for fuseAlgorithmicSteps in "on" "off"
 do
-for nodes in 1
-do
-for tasksPerNode in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 # ham7
-#for tasksPerNode in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 # ham6
-do 
-  let tasks=$nodes*$tasksPerNode
-  let coresPerTask=24/$tasksPerNode # ham7
-  #let coresPerTask=16/$tasksPerNode # ham6
+  prefix=$project-$io-$kernels
+  if [ "$fuseAlgorithmicSteps" == "on" ]; then
+    prefix+="-fused"
+  else
+    prefix+="-nonfused"
+  fi
+  prefix+="-$mesh"
 
-  # Create script
-  script=hamilton.slurm-script
-  newScript=hamilton-$io-p$order-n$nodes-t$tasksPerNode-c$coresPerTask-$sharedMem.slurm-script
-  cp $script $newScript
- 
-  sed -i -r 's,ntasks-per-node(\s*)=(\s*)(([0-9]|\.)*),ntasks-per-node\1=\2'$tasksPerNode',' $newScript
-  sed -i -r 's,sharedMem=None,sharedMem='$sharedMem',' $newScript
-  sed -i 's,Euler_ADERDG-no-output,Euler_ADERDG-'$io',g' $newScript
+  for nodes in 1
+  do
+    for tasksPerNode in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 # ham7
+    #for tasksPerNode in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 # ham6
+    do 
+      let tasks=$nodes*$tasksPerNode
+      let coresPerTask=24/$tasksPerNode # ham7
+      #let coresPerTask=16/$tasksPerNode # ham6
 
-  sed -i 's,p3,p'$order',g' $newScript
-  sed -i 's,regular-0,'$mesh',g' $newScript
+      # Create script
+      script=single-node/hamilton.slurm-script
+      newScript=single-node/hamilton-$prefix-p$order-n$nodes-t$tasksPerNode-c$coresPerTask-$sharedMem.slurm-script
+      cp $script $newScript
+     
+      sed -i -r 's,ntasks-per-node(\s*)=(\s*)(([0-9]|\.)*),ntasks-per-node\1=\2'$tasksPerNode',' $newScript
+      sed -i -r 's,sharedMem=None,sharedMem='$sharedMem',' $newScript
+      sed -i 's,Euler_ADERDG-no-output,Euler_ADERDG-'$io',g' $newScript
 
-  sed -i 's,tasksPerNode=1,tasksPerNode='$tasksPerNode',' $newScript
-  sed -i 's,coresPerTask=1,coresPerTask='$coresPerTask',' $newScript
+      sed -i 's,p3,p'$order',g' $newScript
+      sed -i 's,regular-0,'$mesh',g' $newScript
 
-  sed -i 's,script=hamilton.slurm-script,script='$newScript',g' $newScript 
+      sed -i 's,tasksPerNode=1,tasksPerNode='$tasksPerNode',' $newScript
+      sed -i 's,coresPerTask=1,coresPerTask='$coresPerTask',' $newScript
 
-  # Create spec file
-  spec=Euler_ADERDG-$io.exahype
-  prefix=Euler_ADERDG-$io-p$order-$mesh-t$tasksPerNode-c$coresPerTask # TODO(Dominic): Update!
-  newSpec=$prefix'.exahype'
-  cp $spec $newSpec
+      sed -i 's,script=hamilton.slurm-script,script='$newScript',g' $newScript 
 
-  sed -i -r 's,end-time(\s*)=(\s*)(([0-9]|\.)*),end-time\1=\2'$t',' $newSpec
-  sed -i -r 's,ranks_per_node:([0-9]+),ranks_per_node:'$tasksPerNode',g' $newSpec 
-  sed -i -r 's,cores(\s+)=(\s+)([0-9]+),cores\1=\2'$coresPerTask',g' $newSpec
- 
-  sed -i -r 's,skip-reduction-in-batched-time-steps(\s*)=(\s*)(\w+),skip-reduction-in-batched-time-steps\1=\2'$skipReductionInBatchedTimeSteps',g' $newSpec
-  sed -i -r 's,timestep-batch-factor(\s*)=(\s*)(([0-9]|\.)+),timestep-batch-factor\1=\2'$batchFactor',g' $newSpec
- 
-  sed -i -r 's,order(\s+)const(\s+)=(\s+)([0-9]+),order\1const\2=\3'$order',g' $newSpec
-  sed -i -r 's,maximum-mesh-size(\s*)=(\s*)(([0-9]|\.)*),maximum-mesh-size\1=\2'$h',g' $newSpec
-  
-done
-done
+      # Create spec file
+      spec=single-node/Euler_ADERDG-$io.exahype
+      prefix=single-node/$prefix-p$order-t$tasksPerNode-c$coresPerTask
+      newSpec=$prefix'.exahype'
+      cp $spec $newSpec
+
+      sed -i -r 's,end-time(\s*)=(\s*)(([0-9]|\.)*),end-time\1=\2'$t',' $newSpec
+      sed -i -r 's,ranks_per_node:([0-9]+),ranks_per_node:'$tasksPerNode',g' $newSpec 
+      sed -i -r 's,cores(\s+)=(\s+)([0-9]+),cores\1=\2'$coresPerTask',g' $newSpec
+     
+      sed -i -r 's,skip-reduction-in-batched-time-steps(\s*)=(\s*)(\w+),skip-reduction-in-batched-time-steps\1=\2'$skipReductionInBatchedTimeSteps',g' $newSpec
+      sed -i -r 's,timestep-batch-factor(\s*)=(\s*)(([0-9]|\.)+),timestep-batch-factor\1=\2'$batchFactor',g' $newSpec
+     
+      sed -i -r 's,order(\s+)const(\s+)=(\s+)([0-9]+),order\1const\2=\3'$order',g' $newSpec
+      sed -i -r 's,maximum-mesh-size(\s*)=(\s*)(([0-9]|\.)*),maximum-mesh-size\1=\2'$h',g' $newSpec
+    done
+  done
 done
