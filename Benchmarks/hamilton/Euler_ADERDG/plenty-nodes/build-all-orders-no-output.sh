fd8301090ae3f37a33a58c547442e952fb29829c
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-08-10 13:14:38 +0100

    plenty-nodes: Fix bug in script.

--- Benchmarks/hamilton/Euler_ADERDG/plenty-nodes/build-all-orders-no-output.sh
+++ Benchmarks/hamilton/Euler_ADERDG/plenty-nodes/build-all-orders-no-output.sh
@@ -19,7 +19,7 @@ do
   echo "SHAREDMEM=$SHAREDMEM"
   #read -p "press any key..."
 
-  for p in 5 3 7 9
+  for p in 3 5 7 9
   do 
     rm *.o
     sed -i -r 's,order(\s+)const(\s+)=(\s+)([0-9]+),order\1const\2=\3'$p',' $spec
@@ -31,4 +31,4 @@ do
 done
 
 # restore original file
-mv ${spec}_tmp $Spec
+mv ${spec}_tmp $spec
