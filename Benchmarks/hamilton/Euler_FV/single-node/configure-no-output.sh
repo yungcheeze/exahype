4ba27d198eb4261a7735c457fed1210e1ee74683
domcha <dominic.charrier@googlemail.com>
2017-08-15 11:59:39 +0100

    Euler_FV/single-node: Fix configure scripts.

--- Benchmarks/hamilton/Euler_FV/single-node/configure-no-output.sh
+++ Benchmarks/hamilton/Euler_FV/single-node/configure-no-output.sh
@@ -1,4 +1,4 @@
 echo "Configure project for single-node scaling test (no output)."
-( cd ../../../ && java -jar Toolkit/dist/ExaHyPE.jar --not-interactive Benchmarks/hamilton/Euler_ADERDG/single-node/Euler_ADERDG-no-output.exahype )
+( cd ../../../ && java -jar Toolkit/dist/ExaHyPE.jar --not-interactive Benchmarks/hamilton/Euler_FV/single-node/Euler_FV-no-output.exahype )
 mkdir single-node/results
 rm *.o
--- Benchmarks/hamilton/Euler_FV/single-node/configure-output.sh
+++ Benchmarks/hamilton/Euler_FV/single-node/configure-output.sh
@@ -1,4 +1,4 @@
 echo "Configure project for single-node scaling test (output)."
-( cd ../../../ && java -jar Toolkit/dist/ExaHyPE.jar --not-interactive Benchmarks/hamilton/Euler_ADERDG/single-node/Euler_ADERDG-output.exahype )
+( cd ../../../ && java -jar Toolkit/dist/ExaHyPE.jar --not-interactive Benchmarks/hamilton/Euler_FV/single-node/Euler_FV-output.exahype )
 mkdir single-node/results
 rm *.o
