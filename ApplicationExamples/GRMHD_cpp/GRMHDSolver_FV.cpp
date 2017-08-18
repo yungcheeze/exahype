aa658862306b03e0b63acf9851da3feb35b4af64
Sven K <sven@nils.local>
2017-08-18 14:03:55 +0200

    GRMHD_cpp: Use 9 variables and 11 material parameters.
    Using material parameters saves us setting a lot of nonneccessary zeros in the flux and source.
    
    These material parameters contain the fixed spacetime curvature (4-metric).

--- ApplicationExamples/GRMHD_cpp/GRMHDSolver_FV.cpp
+++ ApplicationExamples/GRMHD_cpp/GRMHDSolver_FV.cpp
@@ -10,7 +10,6 @@ using namespace GRMHD;
 tarch::logging::Log GRMHD::GRMHDSolver_FV::_log( "GRMHD::GRMHDSolver_FV" );
 
 void GRMHD::GRMHDSolver_FV::init(std::vector<std::string>& cmdlineargs, exahype::Parser::ParserView& constants) {
-  // @todo Please implement/augment if required
 }
 
 bool GRMHD::GRMHDSolver_FV::useAdjustSolution(const tarch::la::Vector<DIMENSIONS, double>& center, const tarch::la::Vector<DIMENSIONS, double>& dx, const double t, const double dt) const {
@@ -18,19 +17,24 @@ bool GRMHD::GRMHDSolver_FV::useAdjustSolution(const tarch::la::Vector<DIMENSIONS
 }
 
 void GRMHD::GRMHDSolver_FV::adjustSolution(const double* const x,const double w,const double t,const double dt, double* Q) {
-  // TODO: CALL ID CODE HERE
+	// TODO: CALL ID CODE HERE.
+	// Set the 9 SRMHD variables (D,S_j,tau,B^j) and the 11 ADM material parameters (N^i,g_ij,detg)
 }
 
 exahype::solvers::Solver::RefinementControl GRMHD::GRMHDSolver_FV::refinementCriterion(const double* luh, const tarch::la::Vector<DIMENSIONS, double>& center,const tarch::la::Vector<DIMENSIONS, double>& dx, double t,const int level) {
-  return exahype::solvers::Solver::RefinementControl::Keep;
+	return exahype::solvers::Solver::RefinementControl::Keep;
 }
 
 
 void GRMHD::GRMHDSolver_FV::eigenvalues(const double* const Q, const int dIndex, double* lambda) {
+	// Provide eigenvalues for the 9 SRMHD variables (D,S_j,tau,B^j),
+	// we split off the 11 ADM material parameters (N^i,g_ij,detg)
 	PDE::eigenvalues(Q, dIndex, lambda);
 }
 
 void GRMHD::GRMHDSolver_FV::flux(const double* const Q, double** F) {
+	// Provide fluxes for the 9 SRMHD variables (D,S_j,tau,B^j),
+	// we split off the 11 ADM material parameters (N^i,g_ij,detg)
 	PDE(Q).flux(F);
 }
 
@@ -44,7 +48,7 @@ void GRMHD::GRMHDSolver_FV::boundaryValues(
     const double* const stateInside,
     double* stateOutside) {
 
-  // TODO: SET BV
+	// TODO: SET BV for all 9 variables + 11 parameters.
 }
 
 void GRMHD::GRMHDSolver_FV::fusedSource(const double* const Q, const double* const gradQ, double* S) {
