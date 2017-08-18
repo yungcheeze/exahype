6755d0d090c4b39a46570ba5f1734c8f3fd74bb0
Sven K <sven@nils.local>
2017-08-18 13:46:44 +0200

    GRMHD CPP formulation: Finished so far, proofchecked the formulation.

--- ApplicationExamples/GRMHD_cpp/PDE/PDE.cpp
+++ ApplicationExamples/GRMHD_cpp/PDE/PDE.cpp
@@ -4,6 +4,18 @@
  * Written at 2017-08-06 by SvenK.
  **/
 
+#ifdef TEST_NEW_PDE_AUTONOMOUSLY
+	// in order to autonomously test/copmile this C++ file:
+	#define DIMENSIONS 3
+	namespace GRMHD { constexpr int nVar = 19; }
+	int main() { return 0; }
+#else
+	// If you include to ExaHyPE instead:
+	#include "peano/utils/Dimensions.h" // Defines DIMENSIONS
+	#include "AbstractGRMHDSolver_FV.h" // Defines:
+	namespace GRMHD { constexpr int nVar = GRMHD::AbstractGRMHDSolver_FV::NumberOfVariables; }
+#endif
+
 #include "PDE.h"
 #include "really.h"
 
@@ -14,8 +26,6 @@ void GRMHD::PDE::prepare() {
 	DFOR(i) CONTRACT(j) vel.lo(i)  = gam.lo(i,j) * vel.up(j);
 	// S^i is needed in both flux and ncp
 	DFOR(i) CONTRACT(j) Si.up(i)   = gam.up(i,j) * Si.lo(j);
-
-	// TODO Clarify: Do we store beta up or beta lo
 	
 	WW = SQ(Dens/rho); // W^2
 	BmagBmag = 0; CONTRACT(k) BmagBmag += Bmag.lo(k)*Bmag.up(k); // B^2
@@ -34,27 +44,33 @@ void GRMHD::PDE::flux(double** Fluxes) {
 	Up<vec::stored<3>> zeta; DFOR(k) zeta.up(k) = alpha*vel.up(k) - beta.up(k);
 
 	DFOR(k) { // F^k: flux in direction k.
-		ConservedVariables Flux(Fluxes[k]);
+		ConservedVariableShadow Flux(Fluxes[k]);
 		Flux.Dens = Dens * zeta.up(k);
 		DFOR(i) Flux.Si.lo(i) = alpha*Sij.ul(k,i) - beta.up(k)*Si.lo(i);
 		Flux.tau = alpha*(Si.up(k) - vel.up(k)*Dens) - beta.up(k)*tau;
-		DFOR(j) Flux.Bmag.up(j) = zeta.up(k)*Bmag.up(j) - zeta.up(j)*Bmag.up(k) - Bmag.up(k)*beta.up(j);
+		DFOR(j) Flux.Bmag.up(j) = zeta.up(k)*Bmag.up(j) - zeta.up(j)*Bmag.up(k);
+		
+		// Constraint damping contributions:
+		DFOR(j) Flux.Bmag.up(j) -= Bmag.up(k)*beta.up(j);
 		Flux.phi = alpha*Bmag.up(k) - phi*beta.up(k);
 	}
+	
+	// TODO :Zeroize the fluxes in the material parameters!
 }
 
-void GRMHD::PDE::nonConservativeProduct(const double* const gradQ_Data, double* BgradQ_Data) {
-	// Shape: BgradQ(nVar)
-	ConservedVariables BgradQ(BgradQ_Data);
-	const Really::const_shadow gradQ(gradQ_Data, nDim, nVar);
+void GRMHD::PDE::RightHandSide(const double* const gradQ_Data, double* fusedSource_Data) {
+	// This is the fusedSource = -NCP + AlgebraicSource
+
+	ConservedVariableShadow Source(fusedSource_Data);
+	const Gradients grad(gradQ_Data, nVar);
 	
 	// Sij is the 3-Energy-Momentum tensor. We need S^{ij} and S^i_j in the NCP.	
 	UpSym<sym::stored<3>, sym::stored<3>> Sij;
 	DFOR(i) DFOR(j) Sij.up(i,j) = Si.up(i)*vel.up(j) + gam.up(i,j)*ptot - Bmag.up(i)*Bmag.up(j)/WW - BmagVel * vel.up(i) * Bmag.up(j);
 	Sij.ul_from_up(gam);
 
-	// D
-	BgradQ.Dens = 0;
+	// Source for D
+	Source.Dens = 0;
 	
 	// U is the total energy seen by Eulerian observer
 	// D is the density
@@ -64,31 +80,33 @@ void GRMHD::PDE::nonConservativeProduct(const double* const gradQ_Data, double* 
 
 	// S_i
 	DFOR(i) {
-		BgradQ.Si.lo(i) = U * gradQ(i, Qi.lapse);
-		CONTRACT(k) BgradQ.Si.lo(i) -= Si.lo(k) * gradQ(i, Qi.shift.lo(k));
-		CONTRACT2(l,m) BgradQ.Si.lo(i) -= alpha/2. * Sij.up(l,m) * gradQ(i, Qi.gam.lo(l,m));
+		Source.Si.lo(i) = - U * grad.lo(i).alpha;
+		CONTRACT(k) Source.Si.lo(i) += Si.lo(k) * grad.lo(i).beta.up(k);
+		CONTRACT2(l,m) Source.Si.lo(i) += alpha/2. * Sij.up(l,m) * grad.lo(i).gam.lo(l,m);
 	}
 	
 	// tau
-	CONTRACT(k) BgradQ.tau = Si.up(k) * gradQ(k, Qi.lapse); // Alejandros term
+	CONTRACT(k) Source.tau = -Si.up(k) * grad.lo(k).alpha; // Alejandros term
 	// further BHAC terms for tau:
-	CONTRACT2(i,j) BgradQ.tau -= Sij.ul(j,i) * gradQ(j, Qi.shift.lo(i)); // check: shift_lo!? shift_up!!!
-	CONTRACT3(i,k,j) BgradQ.tau -= alpha/2. * Sij.up(i,k)*beta.up(k) * gradQ(j, Qi.gam.lo(i,k));
+	CONTRACT2(i,j) Source.tau += Sij.ul(j,i) * grad.lo(j).beta.up(i);
+	CONTRACT3(i,k,j) Source.tau += 1./2. * Sij.up(i,k)*beta.up(j) * grad.lo(j).gam.lo(i,k);
+	
+	// Starting from here:
+	// Divergence Cleaning/Constraint damping sources for B^j and Phi.
+	// In case no cleaning would be applied, Source.Bmag == Source.phi == 0.
 	
 	// B^i magnetic field
 	DFOR(i) {
-		BgradQ.Bmag.up(i) = 0;
-		CONTRACT(k) BgradQ.Bmag.up(i) += Bmag.up(k) * gradQ(k, Qi.shift.lo(i));
-		CONTRACT(j) BgradQ.Bmag.up(i) += alpha * gam.up(i,j) * gradQ(j, Qi.Phi);
+		Source.Bmag.up(i) = 0;
+		CONTRACT(k) Source.Bmag.up(i) -= Bmag.up(k) * grad.lo(k).beta.up(i);
+		CONTRACT(j) Source.Bmag.up(i) -= alpha * gam.up(i,j) * grad.lo(j).phi;
 	}
 	
 	// phi
-	const double kappa = 5;
-	BgradQ.phi = - alpha * kappa * phi; // algebraic source
-	CONTRACT(k) BgradQ.phi -= Bmag.up(k) * gradQ(k, Qi.lapse);
-	CONTRACT(k) BgradQ.phi += phi * gradQ(k, Qi.shift.lo(k));
-	                             //   CHECK: Bmag.up(k) or shift.up(k)???
-	CONTRACT3(k,l,m) BgradQ.phi += alpha/2 * Bmag.up(k) * gam.up(l,m) * gradQ(k, Qi.gam.lo(l,m));
+	Source.phi = - alpha * damping_term_kappa * phi; // algebraic source
+	CONTRACT(k) Source.phi += Bmag.up(k) * grad.lo(k).alpha;
+	CONTRACT(k) Source.phi -= phi * grad.lo(k).beta.up(k);
+	CONTRACT3(k,l,m) Source.phi += alpha/2 * gam.up(l,m) * beta.up(k) * grad.lo(k).gam.lo(l,m);
 }
 
 /*
@@ -96,10 +114,10 @@ void GRMHD::PDE::algebraicSource(double* Source_data) {
 	ConservedVariables Source(Source_data);
 	// we could put the following term here:
 	// Source.phi = - alpha * kappa * phi; // algebraic source
+	// All other terms should be zero.
 }
-*/
 
-void GRMHD::PDE::fusedSource(/* const double* const Q, */const double* const gradQ_Data, double* Source) {
+void GRMHD::PDE::fusedSource(const double* const gradQ_Data, double* Source) {
 	// Variables Source(Source_data, nVar);
 	// 1. Compute NCP
 	nonConservativeProduct(gradQ_Data, Source);
@@ -110,11 +128,9 @@ void GRMHD::PDE::fusedSource(/* const double* const Q, */const double* const gra
 	// Source.phi = - lapse * kappa * phi;
 	// 4. Done.
 }
+*/
 
 void GRMHD::PDE::eigenvalues(const double* const Q, const int d, double* lambda) {
+	//std::fill_n(lambda, nVar, 1.0);
 	NVARS(m) lambda[m] = 1.0;
-}
-
-int main() {
-	return 0;
 }
--- ApplicationExamples/GRMHD_cpp/PDE/PDE.h
+++ ApplicationExamples/GRMHD_cpp/PDE/PDE.h
@@ -12,13 +12,26 @@
 #ifndef GRMHD_PDE_CPP
 #define GRMHD_PDE_CPP
 
+/* This header file needs the definition of a constexpr int GRMHD::nVar
+ * as well as a preprocessor variable DIMENSIONS.
+ * You should provide this in the including file, for instance:
+ *
+ *   namespace GRMHD { constexpr int nVar = 10; }
+ *   #define DIMENSIONS 3
+ *   #include "PDE.h"
+ *
+ * DIMENSIONS has to be a C preprocessor variable for C macro ifdefs.
+ * As ExaHyPE, there are some places where this code only works for 2 and 3
+ * dimensions.
+ */
+
 #include "tensish.cpph"
 
-constexpr int nVar = 10; //GRMHD::Whatever::NumberOfVariables;
-#define NVARS(i) for(int i=0; i<nVar; i++)
+//namespace GRMHD { constexpr int nVar = 42; }
+#define NVARS(i) for(int i=0; i < GRMHD::nVar; i++)
 
 namespace GRMHD {
-	class State;
+	using namespace tensish; 
 	using sym::delta; // Kronecker symbol
 	
 	/**
@@ -27,152 +40,272 @@ namespace GRMHD {
 	 * by the ExaHyPE java toolkit. However, by defining our own we are
 	 * independent and can make use of the functional notation.
 	 **/
-	struct ConservedIndices { // aka VariableShortcuts
+	struct ConservedIndices {
 		// Conservatives
 		static constexpr int   Dens = 0;
-		static constexpr int   Si_lo(int i) { return 1+i; }
+		static constexpr int   Si_lo = 1;
 		static constexpr int   tau = 4;
 		// Conserved (C2P invariant)
-		static constexpr int   Bmag_up(int i) { return 5+i; }
+		static constexpr int   Bmag_up = 5;    /* length: 3 */
 		static constexpr int   Phi = 8;
 		// material parameters
 		static constexpr int   lapse = 9;
-		static struct { static constexpr int lo(int i) { return 10+i; } } shift; // TODO: really lo?
-		static struct { static constexpr int lo(int i, int j) { return 13+sym::index(i,j); } } gam; // TODO: Really lo? 
+		static constexpr int   shift_lo = 10;  /* length: 3 */
+		static constexpr int   gam_lo = 13;    /* length: 6 */
+		static constexpr int   detg = 19;
 	};
 	
 	struct PrimitiveIndices { // no correspondence in ExaHyPE
 		static constexpr int rho = 0;
-		// TODO: check if up
-		static constexpr int vec_up = 1;
+		static constexpr int vec_up = 1; /* sic */
 		static constexpr int press = 4;
 	};
 	
-	struct PDE; // forward
-	struct Primitives;
-	struct ADMBase;
-	template<typename vector, typename vector_lo, typename vector_up, typename scalar> struct Conserved;
-	
+	/**
+	* Storage for the conserved hydro variables, i.e. the tuple
+	*   Q = (Dens, S_i, tau, B^i, phi).
+	* The structure allows addressing the vector Q with names.
+	* The template allows to specify types for the scalars and 3-vectors inside Q.
+	* See the typedefs below for usable instances of the "Conserved" structure.
+	*
+	* In principle, this structure mimics the ExaHyPE generated variables structure
+	* but in a much more usable manner (exploiting all the substructure of Q).
+	**/
+	template<typename vector, typename vector_lo, typename vector_up, typename scalar>
+	struct Conserved {
+		vector Q; // const double* const
+		const GRMHD::ConservedIndices Qi;
+		
+		// Conserved Scalars
+		scalar &Dens, &tau, &phi; // const double
+		
+		// Conserved vectors
+		vector_lo Si; /* sic */
+		vector_up Bmag; /* sic */
+		
+		constexpr Conserved(vector Q_) :
+			Q(Q_),
+			Qi(),
+			// Conserved scalars
+			Dens (Q[Qi.Dens]),
+			tau  (Q[Qi.tau]),
+			phi  (Q[Qi.Phi]),
+			// Conserved vectors
+			Si   (Q+Qi.Si_lo),
+			Bmag (Q+Qi.Bmag_up)
+			{}
+		
+		void toPrimitives(double* V); // C2P Cons2Prim operation
+	};
+
+	/**
+	 * Access the Conserved Variables Q fully read-only without access to anything
+	 * beyond what's inside Q (no further storage allocated except pointers to Q).
+	 * That is, you cannot access S^i or B_i as we don't compute it. Therefore, we
+	 * say this structure is just "shadowing" Q. You might want to use it like
+	 * 
+	 *    const ConservedConstShadow Q(Q_);
+	 *    double foo = Q.Bmag.up(1) * Q.Dens;
+	 * 
+	 **/
+	typedef GRMHD::Conserved<
+		const double* const,
+		const new_Lo<vec::const_shadow, const double* const>,
+		const new_Up<vec::const_shadow, const double* const>,
+		const double> ConservedConstShadow;
+
+	/**
+	 * The variable version of the ConservedConstShadow: Allows to change all
+	 * entries of Q by their aliases, as well as directly changing the vectors, i.e.
+	 *
+	 *    ConservedVariableShadow Q(Q_);
+	 *    Q.Bmag.up(2) = Q.Dens + 15;
+	 * 
+	 * But not:
+	 * 
+	 *    Q.Bmag.lo(2) = 15; 
+	 * 
+	 * That is, we dont' allow setting (or even getting) B_i or S^j. This can be
+	 * helpful to avoid errors in the formulation.
+	 **/
+	typedef GRMHD::Conserved<
+		double* const,
+		new_Lo<vec::shadow, double* const>,
+		new_Up<vec::shadow, double* const>,
+		double> ConservedVariableShadow;
+		
+	/**
+	 * Access to the full set of conserved variables in a read only fashion which
+	 * however allows you to retrieve B_i and S^j, i.e. we have storage for these
+	 * variables. These two vectors are the only non-constant members of the
+	 * data structure as you need to fill them after construction.
+	 **/
 	typedef GRMHD::Conserved<
 		const double* const,
 		ConstLo<vec::stored<3>,vec::const_shadow>,
 		ConstUp<vec::const_shadow, vec::stored<3>>,
-		const double> ConservedConstants;
-	
+		const double> ConservedConstFull;
+
+	/**
+	 * The variable version of ConservedConstFull: You can change all conserved
+	 * variables as well access the B_i and S^j. Note that
+	 * 
+	 *   Q.Bmag.lo(2) = 15;
+	 * 
+	 * will not change the shadowed double* Q_.
+	 **/
 	typedef GRMHD::Conserved<
 		double* const,
 		StoredLo<vec::stored<3>, vec::shadow>,
 		StoredUp<vec::shadow, vec::stored<3>>,
-		double> ConservedVariables;
-} // namespace GRMHD
+		double> ConservedVariableFull;
+	
 
-// Variables store the actual (conserved) variables in a shorter/better readable/friendlier
-// manner than the exahype generated variables header file
-template<typename vector, typename vector_lo, typename vector_up, typename scalar>
-struct GRMHD::Conserved {
-	vector Q; // const double* const
-	const GRMHD::ConservedIndices Qi;
-	
-	// Conserved Scalars
-	scalar &Dens, &tau, &phi; // const double
-	
-	// Conserved vectors
-	vector_lo Si;
-	vector_up Bmag;
-	
-	Conserved(vector Q_) :
-		Q(Q_),
-		Qi(),
-		// Conserved scalars
-		Dens (Q[Qi.Dens]),
-		tau  (Q[Qi.tau]),
-		phi  (Q[Qi.Phi]),
-		// Conserved vectors
-		Si   (Q+Qi.Si_lo(0)),
-		Bmag (Q+Qi.Bmag_up(0))
-		{}
-	
-	void toPrimitives(double* V); // C2P Cons2Prim operation
-};
-
-struct GRMHD::Primitives { // are always read only in our formulation, no need for P2C after ID
-	double V[nVar];
-	const GRMHD::PrimitiveIndices Vi;
-	
-	// Primitive Scalars
-	const double &rho, &press;
-	
-	// Primitive vectors
-	ConstUp<vec::const_shadow, vec::stored<3>> vel;
-	
-	Primitives(const double* const Q) :
-		Vi(),
+	/**
+	 * Prmitive Variables.
+	 * 
+	 * They should only store the hydro variables.
+	 * 
+	 **/
+	struct Primitives { // are always read only in our formulation, no need for P2C after ID
+		double V[nVar];
+		const GRMHD::PrimitiveIndices Vi;
+		
 		// Primitive Scalars
-		rho  (V[Vi.rho]),
-		press(V[Vi.press]),
+		const double &rho, &press;
+		
 		// Primitive vectors
-		vel  (V+Vi.vec_up)
-		{
-			//Cons2Prim(Q, V); // TODO include
+		ConstUp<vec::const_shadow, vec::stored<3>> vel; /* vel is vec_up */
+		
+		Primitives(const double* const Q) :
+			Vi(),
+			// Primitive Scalars
+			rho  (V[Vi.rho]),
+			press(V[Vi.press]),
+			// Primitive vectors
+			vel  (V+Vi.vec_up)
+			{
+				//Cons2Prim(Q, V); // TODO include
+			}
+		
+		void toConserved(double* Q); // P2C Prims2Cons operation
+	};
+
+
+	/*
+	// You could define this if you'd like:
+	struct GRMHD::HydroBase : public GRMHD::ConservedVariables, public GRMHD::PrimitiveVariables {
+		HydroBase(const double* const Q) :
+			ConservedVariables(Q),
+			PrimitiveVariables(Q) {}
+	};
+	*/
+
+	template<typename vector_up, typename metric_lo>
+	class ADMBase { // Material Parameters, always read only
+		const GRMHD::ConservedIndices Ai;
+	public:
+		const double &alpha, &detg; // Scalars: Lapse, Determinant of g_ij
+		vector_up beta; // Shift vector: (Conserved) Material parameter vector
+		metric_lo gam;  // 3-Metric: (Conserved) Material parameter tensor
+		
+		constexpr ADMBase(const double* const Q) :
+			Ai(),
+			alpha(Q[Ai.lapse]),
+			detg (Q[Ai.detg]),
+			beta (Q+Ai.shift_lo),
+			gam  (Q+Ai.gam_lo)
+			{}
+		
+		void complete(/* some vector */) {
+			// should (could) provide this.
 		}
+	};
 	
-	void toConserved(double* Q); // P2C Prims2Cons operation
-};
-
-/*
-// You could define this if you'd like:
-struct GRMHD::HydroBase : public GRMHD::ConservedVariables, public GRMHD::PrimitiveVariables {
-	HydroBase(const double* const Q) :
-		ConservedVariables(Q),
-		PrimitiveVariables(Q) {}
-};
-*/
-
-class GRMHD::ADMBase { // Material Parameters, always read only
-	const GRMHD::ConservedIndices Ai;
-public:
-	const double &alpha;
-	// (Conserved) Material parameter vector
-	ConstLo<vec::stored<3>, vec::const_shadow> beta;
+	typedef GRMHD::ADMBase<
+		// ConstUp<vec::const_shadow, vec::stored<3>>, // if you ever need beta_lo
+		new_Up<vec::const_shadow, const double* const>, // if you never need beta_lo
+		metric3> FullADMBase;
 	
-	// (Conserved) Material parameter tensor
-	metric3 gam;
-	
-	ADMBase(const double* const Q) :
-		Ai(),
-		alpha(Q[Ai.lapse]),
-		beta (Q+Ai.shift.lo(0)),
-		gam  (Q+Ai.gam.lo(0,0))
-		{}
-	
-	void complete(/* some vector */) {
-		// should provide this.
-	}
-};
+	// A version of the ADMVariables where beta_lo and the upper metric
+	// are not recovered.
+	typedef GRMHD::ADMBase<
+		const new_Up<vec::const_shadow, const double* const>,
+		const new_Lo<sym::const_shadow, const double* const>
+		> BasicADMBase;
 
 
-struct GRMHD::PDE : public GRMHD::ConservedConstants, public GRMHD::Primitives, public GRMHD::ADMBase {
-	// 3-Energy momentum tensor
-	// Full<sym::stored<3>, sym::stored<3>> Sij;
-	
-	PDE(const double*const Q_) :
-		ConservedConstants(Q_),
-		Primitives(Q_),
-		ADMBase(Q_)
-		{ prepare(); }
+	// A type storing the gradients of the conserved vector in one direction.
+	// Since it does not make sense to
+	// retrieve the lower/upper components of gradients, we don't even allocate storage or
+	// provide conversion strategies.
+	struct Gradient : public BasicADMBase, public ConservedConstShadow {
+		constexpr Gradient(const double* const Q) : BasicADMBase(Q), ConservedConstShadow(Q) {}
+	};
 
-	// Quantities for computing the energy momentum tensor
-	double WW, BmagBmag, BmagVel, ptot;
-	void prepare(); // prepare that shit
-	
-	void flux(/* const double* const Q, */ double** Fluxes);
-	void nonConservativeProduct(/* const double* const Q, */ const double* const gradQ_Data, double* BgradQ_Data);
-	void algebraicSource(/* const double* const Q, */ double* Source_data);
-	void fusedSource(/* const double* const Q, */const double* const gradQ_Data, double* Source_data);
-	
-	// currently trivial:
-	static void eigenvalues(const double* const Q, const int d,double* lambda);
-};
+	// Gradients in each direction: partial_i * something
+	struct Gradients {
+		const Gradient dir[DIMENSIONS];
+		// Access an element in some direction
+		const Gradient& operator[](int i) const { return dir[i]; }
+		// Access an element, indicating that it's partial_i
+		const Gradient& lo(int i) const { return dir[i]; }
+		
+		Gradients(const double* const gradQ, const int nVar) :
+			#if DIMENSIONS == 3
+			dir{gradQ+0, gradQ+nVar, gradQ+2*nVar}
+			#elif DIMENSIONS == 2
+			dir{gradQ+0, gradQ+nVar}
+			#endif
+			// function body:
+			{}
+	};
 
+	/**
+	 * The PDE structure wraps ("shadows") the read-only conserved vector Q in order to provide
+	 * the possibility to compute the flux and the source terms. It inherits the conserved
+	 * variables, primitive variables and ADM variables for convenient access when implementing
+	 * the source and the flux.
+	 *
+	 * It used to provide storage for the 3-Energy momentum tensor in the past, but now this is
+	 * stored directly in the methods because they need different forms (S_ij, S^ij or S^i_j)
+	 * which we address differently in the current formalism.
+	 **/
+	struct PDE : public ConservedConstFull, public Primitives, public FullADMBase {
+		// 3-Energy momentum tensor
+		// Full<sym::stored<3>, sym::stored<3>> Sij;
+		
+		// Constraint damping constant
+		const double damping_term_kappa = 5;
+		
+		PDE(const double*const Q_) :
+			ConservedConstFull(Q_),
+			Primitives(Q_),
+			ADMBase(Q_)
+			{ prepare(); }
 
+		// Quantities for computing the energy momentum tensor
+		double WW, BmagBmag, BmagVel, ptot;
+		
+		/// Prepares the Conserved B_i, S^i as well as the quantities neccessary to compute
+		/// The energy momentum tensor.
+		void prepare();
+		
+		/// This is the algebraic conserved flux. Chainable.
+		void flux(/* const double* const Q, */ double** Fluxes);
+		
+		/// This is the fusedSource, but we just call it RightHandSide because it is on the RHS
+		/// of the PDE. Chainable.
+		void RightHandSide(/* const double* const Q, */const double* const gradQ_Data, double* Source_data);
+		
+		// You can recover these functions by just reworking what's in the fusedSource.
+		//void nonConservativeProduct(/* const double* const Q, */ const double* const gradQ_Data, double* BgradQ_Data);
+		//void algebraicSource(/* const double* const Q, */ double* Source_data);
+		
+		// currently trivial. Since we don't need to access Q, we provide static
+		// access in order to avoid unneccessary boilerplate work.
+		static void eigenvalues(const double* const Q, const int d,double* lambda);
+	};
+
+} // namespace GRMHD
 #endif /* GRMHD_PDE_CPP */
