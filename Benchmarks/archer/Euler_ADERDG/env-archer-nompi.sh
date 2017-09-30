9292fd1a09c2a5379b541749f83ad4cf6a27b8ae
tobias <tobias@localhost>
2017-09-30 10:36:15 +0100

    added

---
+++ Benchmarks/archer/Euler_ADERDG/env-archer-nompi.sh
@@ -0,0 +1,12 @@
+module load java/jdk1.8.0_51
+
+export EXAHYPE_CC=icpc
+
+export MODE=Release
+export COMPILER=Intel
+export DISTRIBUTEDMEM=None
+export ARCHITECTURE=hsw
+export GPROF=off
+
+# optimised kernels
+export USE_IPO=on
