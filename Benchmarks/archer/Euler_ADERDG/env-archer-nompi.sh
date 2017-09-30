ccc4f9c4baf77171d728655e24722bcdd0a580b0
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2017-09-30 11:01:50 +0100

    fixed pathes

--- Benchmarks/archer/Euler_ADERDG/env-archer-nompi.sh
+++ Benchmarks/archer/Euler_ADERDG/env-archer-nompi.sh
@@ -1,12 +1,17 @@
 module load java/jdk1.8.0_51
 
+module swap PrgEnv-cray PrgEnv-intel
+
 export EXAHYPE_CC=icpc
 
 export MODE=Release
 export COMPILER=Intel
 export DISTRIBUTEDMEM=None
-export ARCHITECTURE=hsw
+export ARCHITECTURE=knl
 export GPROF=off
 
 # optimised kernels
 export USE_IPO=on
+export TBB_SHLIB="-L/opt/intel/compilers_and_libraries_2017.0.098/linux/tbb/lib/mic -ltbb"
+
+
