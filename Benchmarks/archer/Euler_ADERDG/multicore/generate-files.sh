ac913f8ccaca118b8c9646eae558905b2f928447
Tobias Weinzierl <tobias.weinzierl@durham.ac.uk>
2017-10-01 19:42:35 +0100

    scripts seem to work in interactive mode at least

--- Benchmarks/archer/Euler_ADERDG/multicore/generate-files.sh
+++ Benchmarks/archer/Euler_ADERDG/multicore/generate-files.sh
@@ -63,9 +63,7 @@ do
   cp $script $newScript
  
   sed -i 's,'$project'-no-output-regular-0,'$prefix',g' $newScript
-
   sed -i 's,p3,p'$order',g' $newScript
-
   sed -i 's,script=multicore/archer.pbs,script='$newScript',g' $newScript 
   
   # Create spec files
