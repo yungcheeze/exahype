c81bf654917eed66230c6f62c020977819df5533
Dominic Etienne Charrier <dominic.e.charrier@durham.ac.uk>
2017-10-09 13:25:06 +0100

    Some modifications of the hamilton tests.

--- Benchmarks/hamilton/Euler_ADERDG/multicore/generate-files.sh
+++ Benchmarks/hamilton/Euler_ADERDG/multicore/generate-files.sh
@@ -24,7 +24,7 @@ batchFactor=0.8
 hMax=( 0.03704 0.01235 0.00412 0.00138 0.00046 ) # 1/3^l ceiled with significance 1e-5
 io=no-output # or output
 
-kernels=gen # this is just an identifier; actual kernels must be chosen before building the executables
+kernels=opt # this is just an identifier; actual kernels must be chosen before building the executables
 
 # Derived options
 
@@ -45,15 +45,15 @@ prefix+="-$mesh"
 for order in 3 5 7 9
 do
   # SIMULATION END TIME
-  T=( 0.01 0.00334 0.00112 0.00038 0.00013 )            # p=3
+  T=(  0.00333333333334 0.00111111111112 0.00037037037038 0.000123456790124 0.0000411522633746  )            # p=3
   if (( order == 5 )); then
-    T=( 0.006364 0.002126 0.000713 0.000242 0.000083 )  # p=5; (2*3+1)/(2*order+1)*T_3 ceiled with sig. 1e-6
+    T=( 0.00212121212122 0.000707070707072 0.0002356902357 0.0000785634118968 0.0000261878039657 )  # p=5; (2*3+1)/(2*order+1)*T_3 ceiled with sig. 1e-6
   fi
   if (( order == 7 )); then
-    T=( 0.004667 0.001559 0.000523 0.000178 0.000061 )  # p=7
+    T=( 0.0015555555555587 0.0005185185185227 0.0001728395061773 0.0000576131687245 0.0000192043895748 ) # p=7
   fi
   if (( order == 9 )); then
-    T=( 0.003685 0.001231 0.000413 0.00014 0.000048 )   # p=9
+    T=(0.0012280701754411 0.0004093567251495 0.0001364522417189 0.0000454840805720 0.0000151613601906 ) # p=9
   fi
   t=${T[i]}
   
@@ -69,8 +69,8 @@ do
   sed -i 's,script=multicore/hamilton.slurm-script,script='$newScript',g' $newScript 
   
   # Create spec files
-  for coresPerTask in 1 12 24
-  #for coresPerTask in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 48 # ham7
+  #for coresPerTask in 1 12 24
+  for coresPerTask in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
   #for coresPerTask in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 32 # ham6
   do
     spec=multicore/Euler_ADERDG-$io.exahype
